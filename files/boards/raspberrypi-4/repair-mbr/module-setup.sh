bb6bae452a911adf720dbfdb4cf2e6459f2cce61
Valentin David <valentin.david@codethink.co.uk>
2020-05-18 16:03:44 +0200

    Add support for dracut+ostree in rpi4 image

---
+++ files/boards/raspberrypi-4/repair-mbr/module-setup.sh
@@ -0,0 +1,20 @@
+check() {
+    return 0
+}
+
+depends() {
+    echo systemd
+    echo eos-repartition
+}
+
+install() {
+    dracut_install sfdisk
+    dracut_install sed
+
+    inst_script "$moddir/repair-mbr.sh" /bin/repair-mbr
+    inst_simple "$moddir/repair-mbr.service" \
+		"$systemdsystemunitdir/repair-mbr.service"
+    mkdir -p "${initdir}/$systemdsystemunitdir/initrd.target.wants"
+    ln_r "$systemdsystemunitdir/repair-mbr.service" \
+	 "$systemdsystemunitdir/initrd.target.wants/repair-mbr.service"
+}
