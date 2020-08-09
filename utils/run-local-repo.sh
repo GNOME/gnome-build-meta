4d5c4d5273c4950c42695dc03f21ff893d2eb9a6
Abderrahim Kitouni <akitouni@gnome.org>
2020-08-09 14:10:01 +0100

    utils/run-local-repo.sh: use caddy or webfsd in preference to python http.server

--- utils/run-local-repo.sh
+++ utils/run-local-repo.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-exec python3 -m http.server 8000 --directory ostree-repo
+PORT=8000
+DIRECTORY=ostree-repo
+
+if type -p caddy > /dev/null; then
+    if caddy -version > /dev/null; then
+        echo "Found caddy v1"
+        exec caddy -port $PORT -root $DIRECTORY
+    else
+        echo "Found caddy v2"
+        exec caddy file-server --listen :$PORT --root $DIRECTORY
+    fi
+elif type -p webfsd > /dev/null; then
+    echo "Found webfsd"
+    exec webfsd -F -l - -p $PORT -r $DIRECTORY
+else
+    echo "Running using python web server, please install caddy or webfs instead."
+    exec python3 -m http.server $PORT --directory $DIRECTORY
+fi
