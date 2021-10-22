7373400e183c7e4f9aae2bd1c94390bb183b2235
Fabian Orccon <cfoch.fabian@gmail.com>
2021-10-22 03:41:15 +0200

    openqa: Save state of resulting installation as an image

--- openqa/tests/gnome_desktop.pm
+++ openqa/tests/gnome_desktop.pm
@@ -6,6 +6,11 @@ sub run {
     my $self = shift;
     assert_and_click('gnome_desktop_tour', timeout => 60, 'left');
     assert_screen('gnome_desktop_desktop', 60);
+
+    send_key("alt-f2", 1);
+
+    type_string("poweroff\n");
+    assert_shutdown(180);
 }
 
 sub test_flags {
