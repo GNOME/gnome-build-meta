9270945e5f28c2a28d7f7978de258ba134d1a4f1
Sam Thursfield <sam.thursfield@codethink.co.uk>
2021-11-04 13:08:16 +0100

    openqa: Add tests for 5 more apps
    App tests are not fatal, ensuring we test all apps even if one or
    more tests don't pass.
    
    Part of https://gitlab.gnome.org/GNOME/gnome-build-meta/-/issues/431

--- openqa/main.pm
+++ openqa/main.pm
@@ -5,9 +5,14 @@ use autotest;
 use needle;
 use File::Basename;
 
-autotest::loadtest "tests/gnome_install.pm";
-autotest::loadtest "tests/gnome_welcome.pm";
-autotest::loadtest "tests/gnome_desktop.pm";
-autotest::loadtest "tests/app_settings.pm";
+autotest::loadtest("tests/gnome_install.pm");
+autotest::loadtest("tests/gnome_welcome.pm");
+autotest::loadtest("tests/gnome_desktop.pm");
+autotest::loadtest("tests/app_cheese.pm");
+autotest::loadtest("tests/app_epiphany.pm");
+autotest::loadtest("tests/app_gnome_weather.pm");
+autotest::loadtest("tests/app_nautilus.pm");
+autotest::loadtest("tests/app_settings.pm");
+autotest::loadtest("tests/app_simple_scan.pm");
 
 1;
