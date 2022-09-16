4aea5185f1ff1fb066cd322e13c93eae4a2bbf17
Sam Thursfield <sam.thursfield@codethink.co.uk>
2022-09-16 11:39:28 +0100

    Add some waiting while typing location
    Otherwise, shift appears for no reason, as in:
    
        https://openqa.gnome.org/tests/128#step/gnome_welcome/5

--- tests/gnome_welcome.pm
+++ tests/gnome_welcome.pm
@@ -9,7 +9,7 @@ sub run {
     assert_and_click('gnome_firstboot_privacy', timeout => 10, button => 'left');
     assert_screen('gnome_firstboot_timezone_1', 30);
     send_key('tab');
-    type_string('London, East');
+    type_string('London, East', wait_screen_change => 6);
     assert_and_click('gnome_firstboot_timezone_2', timeout => 20, button => 'left');
     assert_and_click('gnome_firstboot_timezone_3', timeout => 20, button => 'left');
     assert_and_click('gnome_firstboot_accounts', timeout => 10, button => 'left');
