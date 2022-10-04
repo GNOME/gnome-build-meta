ce9f3c66000164f676b120a5a8a80d238a4e5e3a
Abderrahim Kitouni <abderrahim.kitouni@codethink.co.uk>
2022-10-04 15:49:18 +0200

    Add script to update refs
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/1798>

---
+++ utils/update_refs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+from datetime import datetime
+
+now = datetime.now()
+
+track_elements = [
+    "core.bst",
+    "flatpak-runtimes.bst",
+    "vm/image.bst",
+    "boards/pinebook-pro/image.bst",
+    "boards/pinephone/image.bst",
+    "boards/pinephone-pro/image.bst",
+    "boards/rock64/image.bst",
+    "boards/raspberrypi-4/image.bst",
+    "vm/repo-devel.bst",
+    "iso/image.bst",
+]
+
+bst_command = os.environ.get("BST", "bst").split()
+
+
+def git(*args):
+    return subprocess.check_call(["git"] + list(args))
+
+
+def bst(*args):
+    return subprocess.check_call(bst_command + ["--no-interactive"] + list(args))
+
+
+bst("track", "--deps", "all", *track_elements)
+
+git(
+    "switch",
+    "--force-create",
+    "update-bot/" + now.strftime("%F-%H-%M"),
+)
+
+git("add", "--update", ".")
+
+git("commit", "--message", "Update element refs")
