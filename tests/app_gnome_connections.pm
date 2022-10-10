74746585c445d9a778c13c588ea3c6c8d9f02827
Sam Thursfield <sam.thursfield@codethink.co.uk>
2022-10-10 17:59:42 +0200

    tests/app_gnome_connections.pm: Close the 'Welcome' popup
    Otherwise, the alt-f4 sent by close_app() only seems to close the popup
    and the app stays open during the following tests.

--- tests/app_gnome_connections.pm
+++ tests/app_gnome_connections.pm
@@ -6,7 +6,12 @@ use gnomeutils;
 
 sub run {
     start_app('gnome-connections');
+    assert_screen('app_gnome_connections_welcome', 10);
+
+    # Close the "Welcome to Connections" popup
+    wait_screen_change { send_key('alt-f4') };
     assert_screen('app_gnome_connections_home', 10);
+
     close_app;
 }
 
