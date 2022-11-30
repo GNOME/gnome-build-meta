c1c76920660601a1dae53970e20bb98eb1d2a93c
Sam Thursfield <sam.thursfield@codethink.co.uk>
2022-11-30 15:35:22 +0100

    Modify kernel args so we capture journal output to serial0
    https://gitlab.gnome.org/GNOME/openqa-tests/-/issues/7

--- tests/gnome_install.pm
+++ tests/gnome_install.pm
@@ -4,6 +4,15 @@ use testapi;
 
 sub run {
     my $self = shift;
+
+    assert_screen('gnome_iso_bootloader', timeout => 30);
+    wait_screen_change { send_key('e') };
+    send_key('end');
+    type_string(' console=ttyS0');
+    type_string(' systemd.journald.forward_to_console=1');
+    save_screenshot;
+    send_key('ret');
+
     assert_and_click('gnome_install_1', timeout => 120, button => 'left');
     assert_and_click('gnome_install_disk', timeout => 10, button => 'left');
     assert_and_click('gnome_install_disk2', timeout => 10, button => 'left');
