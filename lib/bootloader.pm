GNOME/gnome-build-meta

commit c925d4df19f8c049ef9e225a0345910eac607c4a

Move bootloader setup to a library

lib/bootloader.pm
@@ -1,0 +1,35 @@
+package bootloader;
+
+use base Exporter;
+use Exporter;
+
+use strict;
+use warnings;
+use testapi qw(diag);
+
+use testapi;
+
+our @EXPORT = qw(
+    bootloader_add_kernel_args
+);
+
+=head2 bootloader_add_kernel_args
+  bootloader_add_kernel_args($args)
+Wait for the bootloader menu to appear, and add $args to the kernel commandline
+before continuing the boot process.
+=cut
+sub bootloader_add_kernel_args {
+    my ($self, $args) = @_;
+
+    assert_screen('gnome_iso_bootloader', timeout => 30);
+
+    wait_screen_change { send_key('e') };
+
+    send_key('end');
+    diag("Add kernel args: $args");
+    type_string($args);
+
+    save_screenshot;
+
+    send_key('ret');
+}

tests/gnome_install.pm
@@ -1,17 +1,12 @@
 use base 'basetest';
 use strict;
 use testapi;
+use bootloader;
 
 sub run {
     my $self = shift;
 
-    assert_screen('gnome_iso_bootloader', timeout => 30);
-    wait_screen_change { send_key('e') };
-    send_key('end');
-    type_string(' console=ttyS0');
-    type_string(' systemd.journald.forward_to_console=1');
-    save_screenshot;
-    send_key('ret');
+    bootloader_add_kernel_args(' console=ttyS0 systemd.journald.forward_to_console=1');
 
     assert_and_click('gnome_install_1', timeout => 120, button => 'left');
     assert_and_click('gnome_install_disk', timeout => 10, button => 'left');

commit 7765c538519121942760b7f57d69bae762dbe190

Restore journal output capture during first boot
Fixes: c925d4df19f8c049ef9e225a0345910eac607c4a

lib/bootloader.pm
@@ -19,15 +19,15 @@ Wait for the bootloader menu to appear, and add $args to the kernel commandline
 before continuing the boot process.
 =cut
 sub bootloader_add_kernel_args {
-    my ($self, $args) = @_;
+    my $bootloader_args = shift;
 
     assert_screen('gnome_iso_bootloader', timeout => 30);
 
     wait_screen_change { send_key('e') };
 
     send_key('end');
-    diag("Add kernel args: $args");
-    type_string($args);
+    diag("Add kernel args: $bootloader_args");
+    type_string($bootloader_args);
 
     save_screenshot;
 