771c79f4076e7776f6dfbdad93435a361cfd002e
Sam Thursfield <sam.thursfield@codethink.co.uk>
2023-03-10 14:36:37 +0000

    Commit the test configuration to Git
    The goal is to make the tests more reproducible outside of the CI
    environment, by having all necessary config variables stored in Git.
    
    The `start_job.sh` script now reads variables from there, but still
    overrides them with some default settings so that gnome-build-meta.git
    CI doesn't immediately break when this change lands.
    
    Part of https://gitlab.gnome.org/GNOME/openqa-tests/-/issues/25

--- utils/start_job.sh
+++ utils/start_job.sh
@@ -3,22 +3,36 @@
 # Start job on $OPENQA_HOST.
 #
 # On success, write the job ID returned by server to stdout.
+#
+# There are three ways to control test configuration:
+#
+# 1. File `config/vars.json` inside the tests directory.
+# 2. The first three positional arguments, which control `WORKER_CLASS`,
+#    `VERSION` and `CASEDIR` settings respectively.
+# 3. Subsequent `KEY=VALUE` positional arguments which can control arbitrary
+#    config values.
 
-set -e
+set -eu
 
-worker_class=$1
-version=$2
+worker_class="$1"
+version="$2"
 casedir="$3"
 
+shift 3
+
+# The values below WORKER_CLASS are duplicated in config/vars.json, we preserve
+# them here for compatibility reasons.
 openqa-cli api --apikey $OPENQA_API_KEY --apisecret $OPENQA_API_SECRET \
   --host $OPENQA_HOST \
   -X POST isos \
-  ARCH=x86_64 \
   CASEDIR="$casedir" \
-  DISTRI=gnomeos \
-  FLAVOR=iso \
   ISO=installer.iso \
   NEEDLES_DIR=$OPENQA_NEEDLES_GIT#$OPENQA_NEEDLES_BRANCH \
+  VERSION=$version \
+  WORKER_CLASS=$worker_class \
+  ARCH=x86_64 \
+  DISTRI=gnomeos \
+  FLAVOR=iso \
   PART_TABLE_TYPE=gpt \
   QEMUCPU=host \
   QEMUCPUS=2 \
@@ -26,6 +40,5 @@ openqa-cli api --apikey $OPENQA_API_KEY --apisecret $OPENQA_API_SECRET \
   QEMUVGA="virtio" \
   UEFI=1 \
   UEFI_PFLASH_CODE=/usr/share/qemu/ovmf-x86_64-code.bin \
-  VERSION=$version \
-  WORKER_CLASS=$worker_class \
+  $@ \
   | tee --append openqa.log | jq -e .ids[0]
