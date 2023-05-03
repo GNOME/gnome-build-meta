86891f646f12b0313eafebb6e45c99368f2700ad
Sam Thursfield <sam.thursfield@codethink.co.uk>
2023-05-03 12:23:03 +0200

    utils/pipeline_report.py: Add `--earlier` option
    This is useful when a pipeline fails to find the last good version.

--- utils/pipeline_report.py
+++ utils/pipeline_report.py
@@ -31,26 +31,43 @@ def argument_parser():
         action="store_true",
         help="Enable detailed logging to stderr",
     )
-    parser.add_argument("pipeline", nargs="?")
+    parser.add_argument(
+        "--earlier",
+        type=int,
+        metavar="N",
+        help="Return pipeline N steps before the specified one",
+    )
+    parser.add_argument(
+        "pipeline",
+        nargs="?",
+        help="Pipeline ID. Leave empty for latest pipeline on default branch."
+    )
     return parser
 
 
 class GitlabAPIHelper:
-    def _json(self, url):
-        response = requests.get(url)
+    def _json(self, url, params=None):
+        response = requests.get(url, params=params)
         response.raise_for_status()
         return response.json()
 
-    def _binary(self, url):
-        response = requests.get(url)
+    def _binary(self, url, params=None):
+        response = requests.get(url, params=params)
         response.raise_for_status()
         return BytesIO(response.content)
 
     def query_latest_pipeline(self):
+        # Get latest pipeline for default project branch ('master').
         return self._json(f"{API_BASE}/projects/{PROJECT_ID}/pipelines/latest")
 
     def query_pipeline(self, pipeline_id):
         return self._json(f"{API_BASE}/projects/{PROJECT_ID}/pipelines/{pipeline_id}")
+
+    def list_pipelines(self, ref=None, updated_before=None):
+        return self._json(
+            f"{API_BASE}/projects/{PROJECT_ID}/pipelines",
+            params=dict(ref=ref, updated_before=updated_before),
+        )
 
     def query_pipeline_jobs(self, pipeline_id):
         return self._json(
@@ -71,38 +88,6 @@ def find_in_list(l, predicate, error_text):
     raise RuntimeError(error_text)
 
 
-def generate_report(api: GitlabAPIHelper, pipeline: dict) -> dict:
-    """Generate the report for a specific pipeline."""
-    jobs = api.query_pipeline_jobs(pipeline["id"])
-    test_s3_image_job = find_in_list(
-        jobs,
-        lambda item: item["name"] == "test-s3-image",
-        "Couldn't find test-s3-image job",
-    )
-    test_s3_image_job_id = test_s3_image_job["id"]
-    log.debug("Found test-s3-image job with ID %s", test_s3_image_job_id)
-
-    artifacts_zip = api.query_job_artifacts(test_s3_image_job_id)
-    with ZipFile(artifacts_zip, "r") as z:
-        with z.open("openqa.log") as f:
-            openqa_status_line = f.readline().decode()
-    openqa_status = json.loads(openqa_status_line)
-    openqa_job_id = openqa_status["ids"][0]
-
-    report = dict(
-        gnome_build_meta_ref=pipeline["ref"],
-        gnome_build_meta_commit_id=pipeline["sha"],
-        gnome_build_meta_commit_date=test_s3_image_job["commit"]["created_at"],
-        gnome_build_meta_commit_title=test_s3_image_job["commit"]["title"],
-        gitlab_pipeline_id=pipeline["id"],
-        gitlab_test_s3_image_job_id=test_s3_image_job_id,
-        gitlab_test_s3_image_job_finished_at=test_s3_image_job["finished_at"],
-        gitlab_test_s3_image_job_status=test_s3_image_job["status"],
-        openqa_job_id=openqa_job_id,
-    )
-    return report
-
-
 TEMPLATE = """
 Repo: gnome-build-meta
 Commit: {gnome_build_meta_commit_id}
@@ -118,8 +103,63 @@ OpenQA job: https://openqa.gnome.org/tests/{openqa_job_id}
 """
 
 
-def print_report_text(report):
-    print(TEMPLATE.format(**report))
+class ScriptHelper:
+    def find_pipeline(self, api, pipeline_id=None, earlier=None, ref='master'):
+        """Find the right pipeline based on the commandline options."""
+        if pipeline_id:
+            pipeline = api.query_pipeline(pipeline_id)
+        else:
+            pipeline = api.query_latest_pipeline()
+            print(f"Latest gnome-build-meta pipeline on default branch is {pipeline['id']}. Status: {pipeline['status']}")
+        if earlier:
+            earlier_pipelines = api.list_pipelines(ref=ref, updated_before=pipeline["updated_at"])
+            log.info(
+                "Finding pipeline %s steps earlier than %s: got %s",
+                earlier, pipeline["id"], len(earlier_pipelines)
+            )
+            if earlier > len(earlier_pipelines):
+                raise RuntimeError(
+                    f"Can't go {earlier} steps earlier than pipeline {pipeline['id']}. "
+                    f"Only {len(earlier_pipelines)} pipelines are available before this one."
+                )
+            earlier_pipeline = earlier_pipelines[-earlier]
+            print(f"Pipeline {earlier} steps than {pipeline['id']} is {earlier_pipeline['id']}. Status: {earlier_pipeline['status']}")
+            return earlier_pipeline
+        return pipeline
+
+    def generate_report(self, api: GitlabAPIHelper, pipeline: dict) -> dict:
+        """Generate the report for a specific pipeline."""
+        jobs = api.query_pipeline_jobs(pipeline["id"])
+        test_s3_image_job = find_in_list(
+            jobs,
+            lambda item: item["name"] == "test-s3-image",
+            "Couldn't find test-s3-image job",
+        )
+        test_s3_image_job_id = test_s3_image_job["id"]
+        log.debug("Found test-s3-image job with ID %s", test_s3_image_job_id)
+
+        artifacts_zip = api.query_job_artifacts(test_s3_image_job_id)
+        with ZipFile(artifacts_zip, "r") as z:
+            with z.open("openqa.log") as f:
+                openqa_status_line = f.readline().decode()
+        openqa_status = json.loads(openqa_status_line)
+        openqa_job_id = openqa_status["ids"][0]
+
+        report = dict(
+            gnome_build_meta_ref=pipeline["ref"],
+            gnome_build_meta_commit_id=pipeline["sha"],
+            gnome_build_meta_commit_date=test_s3_image_job["commit"]["created_at"],
+            gnome_build_meta_commit_title=test_s3_image_job["commit"]["title"],
+            gitlab_pipeline_id=pipeline["id"],
+            gitlab_test_s3_image_job_id=test_s3_image_job_id,
+            gitlab_test_s3_image_job_finished_at=test_s3_image_job["finished_at"],
+            gitlab_test_s3_image_job_status=test_s3_image_job["status"],
+            openqa_job_id=openqa_job_id,
+        )
+        return report
+
+    def print_report_text(self, report):
+        print(TEMPLATE.format(**report))
 
 
 def main():
@@ -130,20 +170,15 @@ def main():
         logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
     api = GitlabAPIHelper()
+    script = ScriptHelper()
 
-    if args.pipeline:
-        pipeline_id = args.pipeline
-        pipeline = api.query_pipeline(pipeline_id)
-    else:
-        pipeline = api.query_latest_pipeline()
-        pipeline_id = pipeline["id"]
-        print(f"Latest gnome-build-meta pipeline on default branch is {pipeline_id}. Status: {pipeline['status']}")
-        if pipeline["status"] == "running":
-            raise RuntimeError("Cannot generate report for a running pipeline.")
-    log.debug("Generate pipeline report for pipeline ID %s", pipeline_id)
+    pipeline = script.find_pipeline(api, args.pipeline, earlier=args.earlier)
+    if pipeline["status"] == "running":
+        raise RuntimeError("Cannot generate report for a running pipeline.")
+    log.debug("Generate pipeline report for pipeline ID %s", pipeline["id"])
 
-    report = generate_report(api, pipeline)
-    print_report_text(report)
+    report = script.generate_report(api, pipeline)
+    script.print_report_text(report)
 
 
 try:
