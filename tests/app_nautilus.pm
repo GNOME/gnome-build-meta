8aef2d7f7eef3c009a3a0cd3777c4a9a5835ec03
Reece11 <ereece244@gmail.com>
2023-10-16 21:58:28 +0300

    reviews

--- tests/app_nautilus.pm
+++ tests/app_nautilus.pm
@@ -8,7 +8,7 @@ sub run {
     start_app('nautilus');
     assert_screen('app_nautilus_home', 10);
     send_key('ctrl-1');
-    assert_screen('app_nautilus_list_view', 10);
+    assert_screen('app_nautilus_list_view', 2);
     close_app;
 }
 
