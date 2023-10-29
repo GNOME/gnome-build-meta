1dea6d2c3d0ec57c7208dd0ba25f709b140f2796
Valentin David <me@valentindavid.com>
2023-10-29 19:21:34 +0100

    Configure journald through kernel command line through smbios

--- tests/gnome_install.pm
+++ tests/gnome_install.pm
@@ -1,12 +1,9 @@
 use base 'basetest';
 use strict;
 use testapi;
-use bootloader;
 
 sub run {
     my $self = shift;
-
-    bootloader_add_kernel_args(' console=ttyS0 systemd.journald.forward_to_console=1');
 
     assert_and_click('gnome_install_1', timeout => 120, button => 'left');
     assert_and_click('gnome_install_disk', timeout => 10, button => 'left');
--- utils/get_smbios.sh
+++ utils/get_smbios.sh
@@ -4,18 +4,24 @@ set -eu
 
 script_dir="$(dirname $0)"
 
-journald_conf="$(cat "${script_dir}/journald.conf" | base64 -w 0)"
-# TODO: get rid of sudoers. The problem is we need to restart journald
-# after journald.conf is provided.
-sudoers_conf="$(cat "${script_dir}/sudoers.conf" | base64 -w 0)"
-
 gen_tmpfiles() {
+    # Kept as example if sudo is required in the future.
+    sudoers_conf="$(cat "${script_dir}/sudoers.conf" | base64 -w 0)"
+
     cat <<EOF
-f+~ /etc/systemd/journald.conf 644 root root - ${journald_conf}
 f+~ /etc/sudoers.d/wheel 644 root root - ${sudoers_conf}
 EOF
 }
 
-tmpfiles_config="$(gen_tmpfiles | base64 -w 0)"
+configs=()
 
-echo "type=11,value=io.systemd.credential.binary:tmpfiles.extra=${tmpfiles_config}"
+# enable if sudo is needed
+if false; then
+  tmpfiles_config="$(gen_tmpfiles | base64 -w 0)"
+  configs+=("value=io.systemd.credential.binary:tmpfiles.extra=${tmpfiles_config}")
+fi
+
+cmdline="console=ttyS0 systemd.journald.forward_to_console=1"
+configs+=("value=io.systemd.stub.kernel-cmdline-extra=${cmdline}")
+
+echo "type=11,$(IFS=,; echo "${configs[*]}")"
