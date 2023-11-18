b00f2570b6a8d35641bfa935eab35856dfb4be45
Riya <riyaos2803@gmail.com>
2023-11-19 00:26:43 +0530

    Testing Accessibility features

--- tests/app_settings.pm
+++ tests/app_settings.pm
@@ -7,6 +7,8 @@ use gnomeutils;
 sub run {
     start_app('gnome-control-center');
     assert_screen('app_settings_startup', 10);
+    
+    # ///////// TESTS RELATED TO APPEARANCE ///////////
 
     # Go to appearance section
     assert_and_click('app_settings_appearance_button', timeout => 15, button => 'left');
@@ -15,7 +17,24 @@ sub run {
     wait_still_screen(5);
     # Change theme back to default
     assert_and_click('default_appearance_button', timeout => 15, button => 'left');
-     
+    
+
+    # ///////////// TESTS RELATED TO ACCESSIBILITY ////////////// 
+    
+    type_string('acc');
+    # Go to accessibility Section
+    assert_and_click('app_settings_accessibility_button', timeout => 15, button => 'left');
+    # Go to Seeing Section
+    assert_and_click('app_settings_accessibility_seeing', timeout => 15, button => 'left');
+    # Switch on contrast
+    assert_and_click('app_settings_accessibility_contrast', timeout => 15, button => 'left');
+    # Switch off contrast
+    assert_and_click('app_settings_no_contrast', timeout => 15, button => 'left');
+    # Switch on Large Text
+    assert_and_click('app_settings_accessibility_largetext', timeout => 15, button => 'left');
+    # Switch on Large Text
+    assert_and_click('app_settings_no_largetext', timeout => 15, button => 'left');
+        
    close_app;
 }
 
@@ -24,4 +43,3 @@ sub test_flags {
 }
 
 1;
-
