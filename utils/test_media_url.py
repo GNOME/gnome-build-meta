fdf1cb227f02e43b5d7c3e1b09370d78f1b2f74b
Sam Thursfield <sam.thursfield@codethink.co.uk>
2023-11-21 19:02:02 +0100

    Use the sysupdate images for testing in CI
    The disk size has increased, there is some minimum size
    thresholds that we need to meet, see:
    <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/commit/e8aa4cdcd13a6a07cf0846edbd75e2ba7038ee5b>
    
    Part of https://gitlab.gnome.org/GNOME/openqa-tests/-/issues/66

--- utils/test_media_url.py
+++ utils/test_media_url.py
@@ -7,6 +7,7 @@ group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("--latest", action="store_true", help="Use latest")
 group.add_argument("--tag", type=str, help="Specify tag")
 group.add_argument("--pipeline", type=int, help="Specify pipeline ID")
+parser.add_argument("--sysupdate", action="store_true", help="Use sysupdate variant")
 parser.add_argument(
     "--kind", required=True, choices=["iso", "disk"], help="Specify kind (iso or disk)"
 )
@@ -14,13 +15,21 @@ parser.add_argument(
 args = parser.parse_args()
 
 version = args.tag or args.pipeline
-if args.kind == "iso":
+if args.sysupdate:
     if args.latest:
-        print("https://os.gnome.org/download/latest/gnome_os_installer.iso")
-    else:
-        print(f"https://os.gnome.org/download/{version}/gnome_os_installer_{version}.iso")
-elif args.kind == "disk":
-    if args.latest:
-        print("https://os.gnome.org/download/latest/gnome_os_disk.img.xz")
-    else:
-        print(f"https://os.gnome.org/download/{version}/disk_{version}.img.xz")
+        raise RuntimeError("No 'stable' version of sysupdate image, use `--pipeline`")
+    if args.kind == "iso":
+        print(f"https://os.gnome.org/download/{version}/gnome_os_sysupdate_installer_{version}-x86_64.iso")
+    elif args.kind == "disk":
+        print(f"https://os.gnome.org/download/{version}/disk_sysupdate_{version}-x86_64.img.xz")
+else:
+    if args.kind == "iso":
+        if args.latest:
+            print("https://os.gnome.org/download/latest/gnome_os_installer.iso")
+        else:
+            print(f"https://os.gnome.org/download/{version}/gnome_os_installer_{version}.iso")
+    elif args.kind == "disk":
+        if args.latest:
+            print("https://os.gnome.org/download/latest/gnome_os_disk.img.xz")
+        else:
+            print(f"https://os.gnome.org/download/{version}/disk_{version}.img.xz")
