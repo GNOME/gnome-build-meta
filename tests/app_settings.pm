63f596f628dc617abc46b9b9672956a3679463a5
kizdorothy <kizdorothy@gmail.com>
2023-12-06 10:54:59 +0300

    added more seeing accessibility tests

--- tests/app_settings.pm
+++ tests/app_settings.pm
@@ -26,7 +26,14 @@ sub run {
     assert_and_click('app_settings_accessibility_button', timeout => 15, button => 'left');
     # Go to Seeing Section
     assert_and_click('app_settings_accessibility_seeing', timeout => 15, button => 'left');
-   
+    # Switch on contrast
+    assert_and_click('app_settings_accessibility_contrast', timeout => 15, button => 'left');
+    # Switch off contrast
+    assert_and_click('app_settings_no_contrast', timeout => 15, button => 'left');
+    # Switch on Large Text
+    assert_and_click('app_settings_accessibility_largetext', timeout => 15, button => 'left');
+    # Switch on Large Text
+    assert_and_click('app_settings_no_largetext', timeout => 15, button => 'left');
         
    close_app;
 }
