37f5118ac1ae180c5a3456c8f29f1409a60bfac7
kizdorothy <kizdorothy@gmail.com>
2023-12-11 15:23:30 +0300

    Separate Gnome app tests from a11y tests

--- tests/app_settings.pm
+++ tests/app_settings.pm
@@ -17,24 +17,6 @@ sub run {
     wait_still_screen(5);
     # Change theme back to default
     assert_and_click('default_appearance_button', timeout => 15, button => 'left');
-    
-
-    # ///////// TESTS RELATED TO ACCESSIBILITY ///////////
-    
-    type_string('acc');
-    # Go to accessibility Section
-    assert_and_click('app_settings_accessibility_button', timeout => 15, button => 'left');
-    # Go to Seeing Section
-    assert_and_click('app_settings_accessibility_seeing', timeout => 15, button => 'left');
-    # Switch on contrast
-    assert_and_click('app_settings_accessibility_contrast', timeout => 15, button => 'left');
-    # Switch off contrast
-    assert_and_click('app_settings_no_contrast', timeout => 15, button => 'left');
-    # Switch on Large Text
-    assert_and_click('app_settings_accessibility_largetext', timeout => 15, button => 'left');
-    # Switch on Large Text
-    assert_and_click('app_settings_no_largetext', timeout => 15, button => 'left');
-        
    close_app;
 }
 
