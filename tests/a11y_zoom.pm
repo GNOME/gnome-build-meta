64abb66a3364a9b90f202fd986fbffca0c9298c8
Tanjuate Achaleke <tanjuateachaleke@gmail.com>
2024-01-03 15:14:53 +0100

    made modifications to a11y_zoom test

--- tests/a11y_zoom.pm
+++ tests/a11y_zoom.pm
@@ -5,10 +5,16 @@ use testapi;
 use gnomeutils;
 
 sub run {
+    start_app('epiphany');
+    assert_screen('app_epiphany_home', 10);
+    type_string('https://example.com/');
+    send_key('ret');
+    assert_screen('app_epiphany_example_page', 10);
+    mouse_set(412, 245);
     send_key('alt-super-8');
-    send_key('alt-super-+');
-
     assert_screen('a11y_zoom', 10);
+    send_key('alt-super-8');
+    close_app;
 }
 
 1;
