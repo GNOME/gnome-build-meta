12d057bb1988070cce76ea3bb7551c87392ef7ca
Jordan Petridis <jpetridis@gnome.org>
2024-02-29 19:37:13 +0200

    ci: Fix the way we push OCI images
    Previously we tried reusing the existing FLATPAK_BRANCH
    variable, however it doesn't work for the case when
    we have a qualifeir in it.
    
    Our images are tagged with the plain version number,
    unlike the flatpak brancher where we have to add the beta
    suffix
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/2739>

--- utils/publish-oci-quay.sh
+++ utils/publish-oci-quay.sh
@@ -6,15 +6,15 @@ podman login -u "$OCI_REGISTRY_USER" -p "$OCI_REGISTRY_PASSWORD" "$OCI_REGISTRY"
 
 set -x
 
-tags=("$CI_COMMIT_SHA" "$FLATPAK_BRANCH")
+tags=("$CI_COMMIT_SHA" "$OCI_BRANCH")
 
-if [ "$FLATPAK_BRANCH" = "master" ]; then
+if [ "$OCI_BRANCH" = "master" ]; then
     tags+=("latest" "nightly")
 fi
 
 for tag in "${tags[@]}"; do
     for name in platform sdk core; do
         echo "Uploading $name:$tag"
-        podman push "$OCI_IMAGE_NAME:$name-$FLATPAK_BRANCH" docker://"$OCI_IMAGE_NAME:$name-$tag"
+        podman push "$OCI_IMAGE_NAME:$name-$OCI_BRANCH" docker://"$OCI_IMAGE_NAME:$name-$tag"
     done
 done
