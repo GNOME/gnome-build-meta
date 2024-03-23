9d5abd2eb1347820344eedd239175019b94bb6c0
Jordan Petridis <jpetridis@gnome.org>
2024-03-23 16:32:13 +0200

    ci: Move build template from yaml to a bash script
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/2805>

---
+++ .gitlab-ci/build_elements.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+set -euxo pipefail
+
+TARGETS=(core.bst flatpak-runtimes.bst flatpak-platform-extensions.bst flatpak-platform-extensions-extra.bst flatpak/platform-manifest.bst flatpak/sdk-manifest.bst vm/manifest-devel.bst vm-secure/manifest-devel.bst oci/platform.bst oci/sdk.bst oci/core.bst)
+
+case "${ARCH}" in
+    aarch64)
+        TARGETS+=(vm/filesystem.bst vm/filesystem-devel.bst)
+    ;;
+    x86_64)
+        TARGETS+=(vm/repo.bst vm/repo-devel.bst)
+    ;;
+    i686)
+        TARGETS=(flatpak-runtimes.bst flatpak-platform-extensions.bst flatpak-platform-extensions-extra.bst)
+    ;;
+esac
+
+case "${ARCH}" in
+    aarch64|x86_64)
+        TARGETS+=(vm-secure/build-non-images.bst)
+    ;;
+esac
+
+: ${BST:=bst}
+$BST --max-jobs $(( $(nproc) / 4 )) -o arch "${ARCH}" build "${TARGETS[@]}"
