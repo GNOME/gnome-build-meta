527b51ea900221bf1c6476188d66b1a3684976b3
Jordan Petridis <jpetridis@gnome.org>
2024-03-23 16:36:11 +0200

    Build GNOME OS with x86_64_v3
    We need to keep the flatpak runtime in v1 sadly for now,
    as it's expected to run anywhere. See [1]
    
    However, we can bump the baseline requirment of GNOME OS
    to x86_64_v3 since we already depend on UEFI and soon
    tpm2 hardware which is from a similar hardware era.

--- .gitlab-ci/build_elements.sh
+++ .gitlab-ci/build_elements.sh
@@ -2,25 +2,37 @@
 
 set -euxo pipefail
 
-TARGETS=(core.bst flatpak-runtimes.bst flatpak-platform-extensions.bst flatpak-platform-extensions-extra.bst flatpak/platform-manifest.bst flatpak/sdk-manifest.bst vm/manifest-devel.bst vm-secure/manifest-devel.bst oci/platform.bst oci/sdk.bst oci/core.bst)
+TARGETS_RUNTIME=(flatpak-runtimes.bst flatpak-platform-extensions.bst flatpak-platform-extensions-extra.bst flatpak/platform-manifest.bst flatpak/sdk-manifest.bst)
+TARGETS_GNOMEOS=(core.bst oci/platform.bst oci/sdk.bst oci/core.bst vm/manifest-devel.bst vm-secure/manifest-devel.bst)
 
 case "${ARCH}" in
     aarch64)
-        TARGETS+=(vm/filesystem.bst vm/filesystem-devel.bst)
+        TARGETS_GNOMEOS+=(vm/filesystem.bst vm/filesystem-devel.bst)
     ;;
     x86_64)
-        TARGETS+=(vm/repo.bst vm/repo-devel.bst)
+        TARGETS_GNOMEOS+=(vm/repo.bst vm/repo-devel.bst)
     ;;
     i686)
-        TARGETS=(flatpak-runtimes.bst flatpak-platform-extensions.bst flatpak-platform-extensions-extra.bst)
+        TARGETS_RUNTIME=(flatpak-runtimes.bst flatpak-platform-extensions.bst flatpak-platform-extensions-extra.bst)
     ;;
 esac
 
 case "${ARCH}" in
     aarch64|x86_64)
-        TARGETS+=(vm-secure/build-non-images.bst)
+        TARGETS_GNOMEOS+=(vm-secure/build-non-images.bst)
     ;;
 esac
 
+case "${ARCH}" in
+    x86_64)
+        ARCH_OPT=(-o x86_64_v3 true -o arch ${ARCH})
+    ;;
+    *)
+        ARCH_OPT=(-o arch ${ARCH})
+    ;;
+esac
+
+# Build the runtime with x86_64_v1 and GNOME OS with _v3
 : ${BST:=bst}
-$BST --max-jobs $(( $(nproc) / 4 )) -o arch "${ARCH}" build "${TARGETS[@]}"
+$BST --max-jobs $(( $(nproc) / 4 )) -o arch "${ARCH}" build "${TARGETS_RUNTIME[@]}"
+$BST --max-jobs $(( $(nproc) / 4 )) "${ARCH_OPT[@]}" build "${TARGETS_GNOMEOS[@]}"
