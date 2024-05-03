6e99c0b59c7ace13442135fc35cbe46b917a9b1d
Neill Whillans <neill.whillans@codethink.co.uk>
2024-05-03 14:22:27 +0000

    .gitlab-ci.yml: Improvements to CVE report generation
    - Make use of freedesktop-sdk CVE report generation utils
    - Addtion of vm and vm-secure CVE reports
    - Utility to download CVE report artifacts for supported branches
    - Utility to generate html for a Release Contents page for supported
    branches
    - Publish Release Contents page and CVE reports, to Gitlab Pages, for
    each build of master
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/2873>

---
+++ utils/generate_release_contents.py
@@ -0,0 +1,66 @@
+"""
+Used to generate a Gitlab Page that has links to the CVE reports
+of master and all supported, release branches, that are also stored on pages
+"""
+
+import shutil
+
+import requests
+
+CALENDAR = "https://gitlab.gnome.org/Teams/Websites/release.gnome.org/-/raw/jekyll/_data/calendar.json"
+GITLAB_PAGES_URL = "https://gnome.pages.gitlab.gnome.org/gnome-build-meta"
+GITLAB_PAGES_FILENAME = "release-contents.html"
+
+
+def populate_branch_html(release_contents, branch: str) -> None:
+    """CVE report html template."""
+    html_template = f"""<html>
+    <h3> {branch}</h3>
+    <p><a
+    href="{GITLAB_PAGES_URL}/{branch}/cve-reports/platform.html">Platform CVE Report</a></p>
+    <p><a
+    href="{GITLAB_PAGES_URL}/{branch}/cve-reports/sdk.html">SDK CVE Report</a></p>
+    <p><a
+    href="{GITLAB_PAGES_URL}/{branch}/cve-reports/vm.html">VM CVE Report</a></p>
+    <p><a
+    href="{GITLAB_PAGES_URL}/{branch}/cve-reports/vm-secure.html">Secure VM CVE Report</a></p>
+    """
+    release_contents.write(html_template)
+
+
+def main():
+    """Generates html for page containing links to CVE
+    reports of master and stable branches.
+    """
+    # Obtain release IDs
+    resp = requests.get(CALENDAR, timeout=20)
+    data = resp.json()
+    release_branches = [data["unstable"], data["stable"], data["old_stable"]]
+
+    with open(GITLAB_PAGES_FILENAME, "w", encoding="UTF-8") as release_contents:
+
+        head_template = """<html>
+            <head>
+            <title>Release Contents</title>
+            </head>
+            <body>
+            <h2>CVE Reports</h2>
+        """
+
+        release_contents.write(head_template)
+
+        populate_branch_html(release_contents, "master")
+
+        for release in release_branches:
+            branch = f"gnome-{release}"
+            response = requests.get(
+                f"{GITLAB_PAGES_URL}/{branch}/cve-reports/platform.html", timeout=5
+            )
+            if response.status_code == 200:
+                populate_branch_html(release_contents, branch)
+
+        shutil.move(GITLAB_PAGES_FILENAME, "public/")
+
+
+if __name__ == "__main__":
+    main()
