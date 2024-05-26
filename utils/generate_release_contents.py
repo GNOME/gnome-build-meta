aaae0fdf0df209880719f95387c9cb63bd3d12e0
Abderrahim Kitouni <abderrahim.kitouni@codethink.co.uk>
2024-05-26 09:54:00 +0100

    utils/generate_release_contents.py: Some improvements and simplifications
    * avoid using shutil
    * don't special case master
    * Remove dead links (older branches don't publish vm and vm-secure manifests)
    * use relative paths in links
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/2917>

--- utils/generate_release_contents.py
+++ utils/generate_release_contents.py
@@ -3,30 +3,25 @@ Used to generate a Gitlab Page that has links to the CVE reports
 of master and all supported, release branches, that are also stored on pages
 """
 
-import shutil
-
+import os
 import requests
 
 CALENDAR = "https://gitlab.gnome.org/Teams/Websites/release.gnome.org/-/raw/jekyll/_data/calendar.json"
-GITLAB_PAGES_URL = "https://gnome.pages.gitlab.gnome.org/gnome-build-meta"
-GITLAB_PAGES_FILENAME = "release-contents.html"
+GITLAB_PAGES_FILENAME = "public/release-contents.html"
 
+RELEASE_MANIFESTS = {"platform": "Platform", "sdk": "SDK", "vm": "VM", "vm-secure": "Secure VM"}
 
 def populate_branch_html(release_contents, branch: str) -> None:
     """CVE report html template."""
-    html_template = f"""<html>
-    <h3> {branch}</h3>
-    <p><a
-    href="{GITLAB_PAGES_URL}/{branch}/cve-reports/platform.html">Platform CVE Report</a></p>
-    <p><a
-    href="{GITLAB_PAGES_URL}/{branch}/cve-reports/sdk.html">SDK CVE Report</a></p>
-    <p><a
-    href="{GITLAB_PAGES_URL}/{branch}/cve-reports/vm.html">VM CVE Report</a></p>
-    <p><a
-    href="{GITLAB_PAGES_URL}/{branch}/cve-reports/vm-secure.html">Secure VM CVE Report</a></p>
-    """
-    release_contents.write(html_template)
 
+    if not os.path.exists(f"public/{branch}"):
+        return
+
+    release_contents.write(f"<h3> {branch}</h3>")
+    for name, pretty_name in RELEASE_MANIFESTS.items():
+        path = f"{branch}/cve-reports/{name}.html"
+        if os.path.exists(f"public/{path}"):
+            release_contents.write(f"""<p><a href="{path}">{pretty_name} CVE Report</a></p>""")
 
 def main():
     """Generates html for page containing links to CVE
@@ -35,7 +30,8 @@ def main():
     # Obtain release IDs
     resp = requests.get(CALENDAR, timeout=20)
     data = resp.json()
-    release_branches = [data["unstable"], data["stable"], data["old_stable"]]
+    releases = [data["unstable"], data["stable"], data["old_stable"]]
+    branches = ["master", *(f"gnome-{release}" for release in releases)]
 
     with open(GITLAB_PAGES_FILENAME, "w", encoding="UTF-8") as release_contents:
 
@@ -49,13 +45,9 @@ def main():
 
         release_contents.write(head_template)
 
-        populate_branch_html(release_contents, "master")
-
-        for release in release_branches:
-            branch = f"gnome-{release}"
+        for branch in branches:
             populate_branch_html(release_contents, branch)
 
-        shutil.move(GITLAB_PAGES_FILENAME, "public/")
         print("Release Contents page has been generated and published.")
 
 if __name__ == "__main__":
