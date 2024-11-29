d7e53bb71567a9eaa89983659e400214f6a9e7a0
Jordan Petridis <jpetridis@gnome.org>
2024-11-29 08:52:08 +0200

    utils/update-refs: Automatically skip problematic ref updates
    Add a list of elements to ignore during tracking. This is only
    for elements that we know can't be updated atm due to an issue
    or other changes pending. Keep the list in the script itself
    so we don't have to keep track of them individually.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3268>

--- utils/update-refs.py
+++ utils/update-refs.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import subprocess
 from datetime import datetime
+from typing import List, Tuple
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -11,6 +12,9 @@ parser.add_argument(
 )
 parser.add_argument(
     "--track-boards", help="Track elements used by board images", action="store_true"
+)
+parser.add_argument(
+    "--no-ignore-elements", help="Do not ignore elements with known issues", action="store_true"
 )
 args = parser.parse_args()
 
@@ -26,6 +30,18 @@ core_elements = [
 boards_elements = [
     "boards/pinephone/image.bst",
     "boards/pinephone-pro/image.bst",
+]
+
+# A list of elements to reset/checkout from master, effectively ignoring
+# them from tracking any newer refs.
+#
+# It's common that due to a certain change an element might be blocking others
+# or failing to build for a couple days. Ignore them so our bot can keep
+# updating everything else automatically.
+#
+# Please open an issue or MR before adding to the list.
+ignore_elements: List[Tuple[str, str]] = [
+    ("sdk/pygobject.bst", "Blocked by the girepository-2.0 ports. See https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3236"),
 ]
 
 def git(*args):
@@ -44,6 +60,13 @@ bst("workspace", "close", "--all")
 track_elements = boards_elements if args.track_boards else core_elements
 bst("-o", "x86_64_v3", "true", "source", "track", "--deps", "all", *track_elements)
 bst("source", "track", "--deps", "all", "flatpak-runtimes.bst")
+
+print("Track completed!")
+
+if not args.no_ignore_elements:
+    for (element, reason) in ignore_elements:
+        print(f"Ignoring {element} due to: {reason}")
+        git("restore", f"elements/{element}")
 
 if args.new_branch:
     git(
