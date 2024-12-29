e822c9e63198c160d31fb777328d9b2feec63b45
Jordan Petridis <jpetridis@gnome.org>
2024-12-29 04:56:35 +0200

    ci: Don't override ARCH variable for x86_64
    In f8153cede3b65900d64aab9c196fc7f5bc8361a4 we changed
    the ARCH variable to be X86_64_V3 so we can simplify
    the build_elements scripts by matching on that.
    
    However we are also using the ARCH variable currently
    to determine the filename of the disk images we upload.
    
    Instead of using arch, introduce a new X86_64_V3 and
    check that to determine what targets to build.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3332>

--- .gitlab-ci/build_elements.sh
+++ .gitlab-ci/build_elements.sh
@@ -8,10 +8,14 @@ TARGETS_GNOMEOS=(core.bst vm/manifest-devel.bst vm-secure/manifest-devel.bst vm-
 # Build the runtime with x86_64_v1 and GNOME OS with x86_64_v3
 # We don't need to build gnomeos on i686
 case "${ARCH}" in
-    x86_64_v3)
-        TARGETS=(${TARGETS_GNOMEOS[@]} vm/repo.bst vm/repo-devel.bst oci/platform.bst oci/sdk.bst oci/core.bst)
+    x86_64)
+        if [[ "${X86_64_V3:-0}" == "1" ]]; then
+            TARGETS=(${TARGETS_GNOMEOS[@]} vm/repo.bst vm/repo-devel.bst oci/platform.bst oci/sdk.bst oci/core.bst)
+        else
+            TARGETS=(${TARGETS_RUNTIME[@]})
+        fi
     ;;
-    x86_64|i686)
+    i686)
         TARGETS=(${TARGETS_RUNTIME[@]})
     ;;
     aarch64)
