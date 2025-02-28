ac8b1bfe1d0c4052f664834099fff4af5d36202e
Abderrahim Kitouni <akitouni@gnome.org>
2025-02-28 15:21:57 +0100

    Revert "utils/update-refs: Skip gobject-introspection for now"
    This reverts commit 8480b987dbf27f176e33dc6563262acdb00428d2.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3468>

--- utils/update-refs.py
+++ utils/update-refs.py
@@ -41,7 +41,6 @@ boards_elements = [
 #
 # Please open an issue or MR before adding to the list.
 ignore_elements: List[Tuple[str, str]] = [
-    ("sdk/gobject-introspection.inc", "Vala errors for new elements atm, see https://gitlab.gnome.org/GNOME/vala/-/issues/1586"),
 ]
 
 def git(*args):
