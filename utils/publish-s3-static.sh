79444a0262f42a18727072511ad023a656bae7d5
Jordan Petridis <jpetridis@gnome.org>
2025-03-24 04:22:55 +0200

    ci: Move script for static-publish into bash
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3555>

---
+++ utils/publish-s3-static.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+set -euox pipefail
+
+pipeline_id="$1"
+
+echo "nightly/$pipeline_id/gnome_os_installer_$pipeline_id.iso" > latest-iso
+echo "nightly/$pipeline_id/disk_$pipeline_id-x86_64.img.xz" > latest-x86_64-disk
+echo "nightly/$pipeline_id/disk_$pipeline_id-aarch64.img.xz" > latest-aarch64-disk
+
+aws s3 cp --acl public-read latest-iso s3://gnome-build-meta/latest-iso
+aws s3 cp --acl public-read latest-x86_64-disk s3://gnome-build-meta/latest-x86_64-disk
+aws s3 cp --acl public-read latest-aarch64-disk s3://gnome-build-meta/latest-aarch64-disk
+
+rm -v latest-iso latest-x86_64-disk latest-aarch64-disk
