7fb59ca8d1bf425540e70062e6dee1c597e0eeed
Valentin David <me@valentindavid.com>
2025-04-02 20:06:00 +0200

    gnomeos: Add an installer prototype for the live image
    This add a prototype of installer. This was written in Python just to
    accelerate development, but it should changed from now to something a
    bit better. All the parts are in, so now we can work on testing and
    improving the experience.
    
    For now the installer will on purpose refuse to install if the disk
    has some data. The user will have to open Disks, then select the disk
    to clean up, then "format", and choose "no partitioning". Then the
    installer will allow installation to happen. This is not a problem for
    VMs like Boxes, because the initial disk is usually empty. Once we
    have a proper interface that is clear enough that we are removing
    data, we can remove that limitation.
    
    The installer application will only appear in live, that is if kernel
    parameter `root=live:gnomeos` is set. And polkit will also only allow
    installation from `wheel` users if we are in live mode.
    
    There are 2 modes of installation, either we install on the disk the
    old fashion way, then after reboot gnome-initial-setup will kick in
    again for the installed OS. That is called for now "OEM installation"
    on the installer.
    
    Or by default, it will just write running system into the disk. After
    reboot the data will still be available.
    
    If a TPM 2.0 is present, encryption will happen automatically.  This
    will use PCR 11 signatures, and pcrlock. PCR 7 will not be statically
    enrolled, but pcrlock can take care of that. We initially do not lock
    the UEFI firmare configuration, because the boot entry will be created
    on first reboot. But after first reboot, it will lock itself
    automatically. Also secure boot will be locked automatically once
    after the users enrolls secure boot keys.
    
    If disk encryption happens, then the user will get a recovery password
    printed in the installer.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3601>

--- files/gnomeos/live/generator.c
+++ files/gnomeos/live/generator.c
@@ -18,6 +18,13 @@
 
 #define _cleanup_(f) __attribute__((cleanup(f)))
 
+static void closep(int *p) {
+        if (*p >= 0) {
+                close(*p);
+                *p = -EBADF;
+        }
+}
+
 static void freestr(char **p) {
         if (*p) {
                 free(*p);
@@ -30,6 +37,31 @@ static void fclosep(FILE **p) {
                 fclose(*p);
                 *p = NULL;
         }
+}
+
+static bool generate_unit(const char *generator_path, const char* name, const char *format, ...) __attribute__ ((format (printf, 3, 4)));
+
+static bool generate_unit(const char *generator_path, const char* name, const char *format, ...) {
+        va_list args;
+        _cleanup_(freestr) char* path = NULL;
+        _cleanup_(fclosep) FILE* file = NULL;
+
+        if (0 > asprintf(&path, "%s/%s", generator_path, name)) {
+                kmsg(3, "Could not format string\n");
+                return false;
+        }
+
+        file = fopen(path, "w");
+        if (!file) {
+                kmsg(3, "Cannot open %s: %s\n", path, STRERROR(errno));
+                return false;
+        }
+
+        va_start(args, format);
+        vfprintf(file, format, args);
+        va_end(args);
+
+        return true;
 }
 
 static bool enable_unit(const char *generator_path, const char *from, const char *unit, const char *unit_path) {
@@ -61,6 +93,16 @@ static bool enable_unit(const char *generator_path, const char *from, const char
         }
 
         return true;
+}
+
+static void mark_live() {
+        /*
+         * This is a marker we can easily check in polkit rules.
+         * See files/gnomeos/proto-installer/org.gnome.Installer1.rules
+         */
+        _cleanup_(closep) int fd = -EBADF;
+        mkdir("/run/gnomeos", 0777);
+        fd = open("/run/gnomeos/is-live", O_CREAT|O_WRONLY|O_TRUNC, 0666);
 }
 
 int main(int argc, char *argv[]) {
@@ -100,6 +142,75 @@ int main(int argc, char *argv[]) {
                 file = fopen("/run/systemd/zram-generator.conf.d/zram1.conf", "w");
                 fprintf(file, "[zram1]\nmount-point=/\nfs-type=btrfs\n");
                 fclosep(&file);
+
+                mark_live();
+
+                if (!generate_unit(argv[1], "var-lib-gnomeos-installer\\x2desp.mount",
+                                   "[Unit]\n"
+                                   "BindsTo=dev-gnomeos\\x2dinstaller-esp.device\n"
+                                   "After=dev-gnomeos\\x2dinstaller-esp.device\n"
+                                   "\n"
+                                   "[Mount]\n"
+                                   "What=/dev/gnomeos-installer/esp\n"
+                                   "Where=/var/lib/gnomeos/installer-esp\n"
+                                   "Type=vfat\n"
+                                   "Options=fmask=0177,dmask=0077,ro,nodev,nosuid,noexec,nosymfollow\n"))
+                        return 1;
+
+                if (!generate_unit(argv[1], "var-lib-gnomeos-installer\\x2desp.automount",
+                                   "[Automount]\n"
+                                   "Where=/var/lib/gnomeos/installer-esp\n"
+                                   "TimeoutIdleSec=120\n"))
+                        return 1;
+
+
+                if (!enable_unit(argv[1], "local-fs.target.wants", "var-lib-gnomeos-installer\\x2desp.automount", ".."))
+                        return 1;
+
+                /* This is needed for the installer. We just prepare it, so it is loaded. But we not enable it. */
+                if (!generate_unit(argv[1], "systemd-cryptsetup@root.service",
+                                   "[Unit]\n"
+                                   "Description=Cryptography Setup for %%I\n"
+                                   "\n"
+                                   "DefaultDependencies=no\n"
+                                   "After=cryptsetup-pre.target systemd-udevd-kernel.socket systemd-tpm2-setup-early.service\n"
+                                   "Before=blockdev@dev-mapper-%%i.target\n"
+                                   "Wants=blockdev@dev-mapper-%%i.target\n"
+                                   "IgnoreOnIsolate=true\n"
+                                   "Before=umount.target cryptsetup.target\n"
+                                   "Conflicts=umount.target\n"
+                                   "Wants=dev-gnomeos\\x2dpab\\x2dinstall-root\\x2dluks.device\n" /* BindsTo? */
+                                   "After=dev-gnomeos\\x2dpab\\x2dinstall-root\\x2dluks.device\n"
+                                   "\n"
+                                   "[Service]\n"
+                                   "Type=oneshot\n"
+                                   "RemainAfterExit=yes\n"
+                                   "TimeoutSec=infinity\n"
+                                   "KeyringMode=shared\n"
+                                   "OOMScoreAdjust=500\n"
+                                   "ExecStart=/usr/bin/systemd-cryptsetup attach root /dev/gnomeos-pab-install/root-luks\n"
+                                   "ExecStop=/usr/bin/systemd-cryptsetup detach root\n"))
+                        return 1;
+
+                /* This is needed for the installer. We just prepare it, so it is loaded. But we not enable it. */
+                if (!generate_unit(argv[1], "efi.mount",
+                                   "[Unit]\n"
+                                   "BindsTo=dev-gnomeos\\x2dpab\\x2dinstall-esp.device\n"
+                                   "After=dev-gnomeos\\x2dpab\\x2dinstall-esp.device\n"
+                                   "\n"
+                                   "[Mount]\n"
+                                   "What=/dev/gnomeos-pab-install/esp\n"
+                                   "Where=/efi\n"
+                                   "Type=vfat\n"
+                                   "Options=fmask=0177,dmask=0077,rw,nodev,nosuid,noexec,nosymfollow\n"))
+                        return 1;
+
+                /* This is needed for the installer. We just prepare it, so it is loaded. But we not enable it. */
+                if (!generate_unit(argv[1], "efi.automount",
+                                   "[Automount]\n"
+                                   "Where=/efi\n"
+                                   "TimeoutIdleSec=120\n"))
+                        return 1;
         }
 
         return 0;
