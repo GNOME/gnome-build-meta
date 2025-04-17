67cc0c10ee423fe32bad2a2154d80c261be6b9e5
Valentin David <me@valentindavid.com>
2025-04-17 13:30:46 +0200

    gnomeos: Work-around double part scan for loop of cdrom
    To enable partitions on the cdrom drive (`BLK_DEV_SR`) which is used
    commonly in VMs, we use a loop device. However if we do the scannig
    using `LO_FLAGS_PARTSCAN` when setting the loop device, the kernel
    seems to be confused and scans it twice. Is it because it is hybrid
    ISO?
    
    The double scanning sends a sequence of "add, remove, add" uevents.
    That makes udev start then stop then start again the device unit.
    
    Generated `systemd-veritysetup@usr.service` unit BindsTo
    the devices. Unfortunately, there cases the ExecStart job would start
    before the second `add` event. In those cases, either the unit will
    fail to start, or it will be stopped.
    
    In practice, booting the live image as CD-ROM was failing 5% to 10% of
    the time.
    
    Partx from util-linux scans in user land and uses `BLKPG` to register
    partitions rather than `BLKRRPART`. That avoids the kernel double
    scanning. And then no `remove` uevent is generated.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3648>

--- files/gnomeos/generate-initramfs/modules/50-cdrom/module.sh
+++ files/gnomeos/generate-initramfs/modules/50-cdrom/module.sh
@@ -1,4 +1,5 @@
 install() {
     install_file_at_path "${moddir}/sr-loop@.service" "/usr/lib/systemd/system/sr-loop@.service"
+    install_file_at_path "${moddir}/sr-loop-partscan@.service" "/usr/lib/systemd/system/sr-loop-partscan@.service"
     install_file_at_path "${moddir}/80-sr-loop.rules" "/usr/lib/udev/rules.d/80-sr-loop.rules"
 }
