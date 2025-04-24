06280d79926734c68680b74e62f45c93b7a88c0c
Adrian Vovk <adrianvovk@gmail.com>
2025-04-23 23:08:10 -0400

    Generate gnome-mimeapps.list
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3680>

---
+++ files/gnome-mimeapps/generate.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from pathlib import Path
+from functools import cmp_to_key
+import tomllib
+import os
+import gi
+gi.require_version("GLib", "2.0")
+from gi.repository import Gio
+
+# Parse CLI arguments
+args = ArgumentParser(description="Scan installed applications to generate GNOME's mimeapps.list")
+args.add_argument("quirks", type=Path, help="Quirks file to control output")
+args.add_argument("output", type=Path, help="Location of output file")
+args = args.parse_args();
+
+# Load quirks
+with open(args.quirks, "rb") as f:
+    class wrapped(dict):
+        __getattr__ = dict.get
+    quirks = wrapped(tomllib.load(f))
+
+os.environ["XDG_DATA_DIRS"] = ":".join(quirks.datadirs)
+
+# Load the apps available on the system
+types = {}
+for app in Gio.AppInfo.get_all():
+    if app.get_id().removesuffix('.desktop') in quirks.skip_apps:
+        continue
+    for type in app.get_supported_types():
+        if type in types:
+            types[type] += [app.get_id()]
+        else:
+            types[type] = [app.get_id()]
+
+# Sort
+def _cmp_incubating(a, b):
+    a = a.removesuffix('.desktop')
+    b = b.removesuffix('.desktop')
+
+    if quirks.incubating.get(a) == b:
+        # Incubating app A replaces core app B. A takes priority over B in the
+        # defaults list. Takes priority = appears earlier in the list, so A < B
+        return -1
+    elif quirks.incubating.get(b) == a:
+        return 1 # The opposite situation
+    else:
+        return 0 # These apps aren't related
+
+for _type, apps in types.items():
+    # First sort: ensure reproducible (alphabetical) order
+    apps.sort()
+
+    # Second sort: make incubator apps come before others
+    apps.sort(key = cmp_to_key(_cmp_incubating))
+
+# Apply overrides.
+for ty, override in quirks.override.items():
+    if isinstance(override, list):
+        if len(override) == 0:
+            del types[ty]
+        else:
+            types[ty] = list(map(lambda x: x + '.desktop', override))
+    else:
+        types[ty] = [override + '.desktop']
+
+# Generate the output
+with open(args.output, "w") as output:
+    print("[Default Applications]", file=output)
+    for type in sorted(types):
+        apps = ';'.join(types[type])
+        print(f"{type}={apps}", file=output)
