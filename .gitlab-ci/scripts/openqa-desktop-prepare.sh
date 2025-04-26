5d32a41833979d19d00635de23825bd8f9c2421b
Jordan Petridis <jpetridis@gnome.org>
2025-04-26 12:16:27 +0300

    ci: Move openqa script from yaml to a bash script
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3688>

---
+++ .gitlab-ci/scripts/openqa-desktop-prepare.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -eux
+set -o pipefail
+
+echo "Preparing test media for desktop test"
+if [ -n "$CI_COMMIT_TAG" ]; then
+    S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_COMMIT_TAG}/disk_${CI_COMMIT_TAG}-x86_64.img.xz
+else
+    S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_PIPELINE_ID}/disk_${CI_PIPELINE_ID}-x86_64.img.xz
+fi
+
+mkdir -p /data/factory/hdd
+curl --fail --get --location $S3_DISK_IMAGE_URL --output /data/factory/hdd/disk.img.xz
+unxz /data/factory/hdd/disk.img.xz
+sha256sum /data/factory/hdd/disk.img
+disk_size_gb=15
+dd if=/dev/zero of=/data/factory/hdd/disk.img seek=$disk_size_gb obs=1GB count=0
---
+++ .gitlab-ci/scripts/openqa-script.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+set -eux
+set -o pipefail
+
+git clone "$OPENQA_TESTS_GIT" --branch "$OPENQA_TESTS_BRANCH" --recurse-submodules ./openqa
+echo "Checked out $OPENQA_TESTS_GIT commit $(git -C ./openqa rev-parse HEAD)"
+
+rm /etc/openqa/*
+cat >/etc/openqa/client.conf <<EOF
+[openqa.gnome.org]
+key = $OPENQA_API_KEY
+secret = $OPENQA_API_SECRET
+EOF
+
+# Kludge so we can use `/tests/config/smbios.txt` to locate the smbios file.
+ln -s "$(pwd)/openqa" /tests
+worker_class="qemu_x86_64-${CI_JOB_ID}"
+openqa/utils/setup_worker.sh "${worker_class}"
+/run_openqa_worker.sh &> worker.log &
+
+echo "Starting jobs: "
+echo
+version="master"
+casedir="$(pwd)/openqa"
+for testsuite in $TESTSUITES; do
+    openqa/utils/start_job.sh "${worker_class}" "${version}" "${casedir}" "${testsuite}" > /tmp/job_id
+    echo " * ${testsuite}: $OPENQA_HOST/tests/$(cat /tmp/job_id)"
+    cat /tmp/job_id >> /tmp/all_job_ids
+done
+
+openqa/utils/wait_for_job.sh "$(cat /tmp/all_job_ids)" > /tmp/exit_code
+exit "$(cat /tmp/exit_code)"
