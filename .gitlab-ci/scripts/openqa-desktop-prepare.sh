163068dfaa0750c89a7bb4cdac5bb4071db0b318
Jordan Petridis <jpetridis@gnome.org>
2025-04-27 12:22:43 +0300

    ci: Make openqa prep scripts work with set -u
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3691>

--- .gitlab-ci/scripts/openqa-desktop-prepare.sh
+++ .gitlab-ci/scripts/openqa-desktop-prepare.sh
@@ -4,14 +4,14 @@ set -eux
 set -o pipefail
 
 echo "Preparing test media for desktop test"
-if [ -n "$CI_COMMIT_TAG" ]; then
+if [[ -n "${CI_COMMIT_TAG:-}" ]]; then
     S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_COMMIT_TAG}/disk_${CI_COMMIT_TAG}-x86_64.img.xz
 else
     S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_PIPELINE_ID}/disk_${CI_PIPELINE_ID}-x86_64.img.xz
 fi
 
 mkdir -p /data/factory/hdd
-curl --fail --get --location $S3_DISK_IMAGE_URL --output /data/factory/hdd/disk.img.xz
+curl --fail --get --location "$S3_DISK_IMAGE_URL" --output /data/factory/hdd/disk.img.xz
 unxz /data/factory/hdd/disk.img.xz
 sha256sum /data/factory/hdd/disk.img
 disk_size_gb=15
