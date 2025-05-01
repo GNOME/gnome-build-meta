41a324aa765608db6042fd501df7809d94cbe264
Jordan Petridis <jpetridis@gnome.org>
2025-05-01 21:32:55 +0300

    ci: Remove AWS_ENDPOINT_URL variable
    We no longer use it and its empty now
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3709>

--- .gitlab-ci/scripts/publish-sysupdate-to-s3.sh
+++ .gitlab-ci/scripts/publish-sysupdate-to-s3.sh
@@ -13,7 +13,7 @@ else
 fi
 
 if [ -n "${target_dir}" ] && [ -n "${IMAGE_VERSION}" ]; then
-    aws $AWS_ENDPOINT_URL s3 cp --acl public-read \
+    aws s3 cp --acl public-read \
         "s3://gnome-build-meta/${target_dir}/sysupdate/" update-images/ \
         --recursive --exclude "*" --include "SHA256SUMS.version.*"
 
@@ -23,15 +23,15 @@ if [ -n "${target_dir}" ] && [ -n "${IMAGE_VERSION}" ]; then
         --output "update-images/SHA256SUMS.gpg" \
         --detach-sig "update-images/SHA256SUMS"
 
-    aws $AWS_ENDPOINT_URL s3 sync --acl public-read \
+    aws s3 sync --acl public-read \
         update-images/ "s3://gnome-build-meta/$target_dir/sysupdate/" \
         --exclude "*" --include "*.xz" --include "*.*hash" --include "SHA256SUMS.version.${IMAGE_VERSION}-${ARCH}"
 
     # keep SHA256SUMS files at the end to minimize time for which files are not available
-    aws $AWS_ENDPOINT_URL s3 sync --acl public-read \
+    aws s3 sync --acl public-read \
         --cache-control max-age=1800 \
         update-images/ "s3://gnome-build-meta/$target_dir/sysupdate/" \
         --exclude "*" --include "SHA256SUMS" --include "SHA256SUMS.gpg"
 fi
 
-aws $AWS_ENDPOINT_URL s3 ls --recursive --human-readable s3://gnome-build-meta/
+aws s3 ls --recursive --human-readable s3://gnome-build-meta/
