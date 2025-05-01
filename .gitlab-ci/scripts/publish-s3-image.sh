79d855645e112b6793179ff6013f8d3294107f89
Jordan Petridis <jpetridis@gnome.org>
2025-05-01 21:39:09 +0300

    ci: Move the script of s3-image job outside of yaml
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3709>

---
+++ .gitlab-ci/scripts/publish-s3-image.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -euox pipefail
+
+${BST_NO_PUSH} --max-jobs $(( $(nproc) / 4 )) ${ARCH_OPT} build gnomeos/image.bst gnomeos/live-image.bst
+${BST} ${ARCH_OPT} artifact checkout --hardlinks gnomeos/image.bst --directory secure-image
+${BST} ${ARCH_OPT} artifact checkout --hardlinks gnomeos/live-image.bst --directory live-image
+
+if [ -n "$CI_COMMIT_TAG" ]; then
+    aws s3 cp --acl public-read secure-image/disk.img.xz \
+        "s3://gnome-build-meta/tag/${CI_COMMIT_TAG}/disk_${CI_COMMIT_TAG}-${ARCH}.img.xz"
+    aws s3 cp --acl public-read live-image/disk.iso \
+        "s3://gnome-build-meta/tag/${CI_COMMIT_TAG}/live_${CI_COMMIT_TAG}-${ARCH}.iso"
+else
+    aws s3 cp --acl public-read secure-image/disk.img.xz \
+        "s3://gnome-build-meta/nightly/${CI_PIPELINE_ID}/disk_${CI_PIPELINE_ID}-${ARCH}.img.xz"
+    aws s3 cp --acl public-read live-image/disk.iso \
+        "s3://gnome-build-meta/nightly/${CI_PIPELINE_ID}/live_${CI_PIPELINE_ID}-${ARCH}.iso"
+fi
+
+aws s3 ls --recursive --human-readable s3://gnome-build-meta/
