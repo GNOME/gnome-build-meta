63f003d62f11e601f23ac40350325b5c7d3dcffb
Valentin David <me@valentindavid.com>
2025-05-01 14:04:01 +0200

    .gitlab-ci: Use live image for openqa
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3705>

---
+++ .gitlab-ci/scripts/openqa-prepare.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+set -eux
+set -o pipefail
+
+echo "Preparing test media for desktop test"
+if [[ -n "${CI_COMMIT_TAG:-}" ]]; then
+    S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_COMMIT_TAG}/live_${CI_COMMIT_TAG}-x86_64.iso
+else
+    S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_PIPELINE_ID}/live_${CI_PIPELINE_ID}-x86_64.iso
+fi
+
+mkdir -p /data/factory/iso
+curl --fail --get --location "$S3_DISK_IMAGE_URL" --output /data/factory/iso/disk.iso
+sha256sum /data/factory/iso/disk.iso
