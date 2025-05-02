c5494ea0107b83123e970c91cf12dca60a1cfe4a
Valentin David <me@valentindavid.com>
2025-05-02 10:52:42 +0200

    app_snapshot: Add screen to give portal permission

--- tests/app_snapshot.pm
+++ tests/app_snapshot.pm
@@ -11,6 +11,7 @@ sub run {
     if ($form_factor_postfix eq '_mobile') {
         resize_app_to_mobile;
     }
+    assert_and_click('app_snapshot_permission'.$form_factor_postfix, button => 'left');
     assert_screen('app_snapshot_home'.$form_factor_postfix, 10);
     close_app;
 }
