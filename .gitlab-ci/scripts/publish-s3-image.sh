0ab964eb334b2c5bc7b39d0056010cb22d146d9e
Jordan Petridis <jpetridis@gnome.org>
2025-05-07 15:52:18 +0300

    ci: Combine the image build jobs
    Now that we dropped ostree and have a single
    variant of the image, we can combine the jobs.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3725>

--- .gitlab-ci/scripts/publish-s3-image.sh
+++ .gitlab-ci/scripts/publish-s3-image.sh
@@ -2,9 +2,6 @@
 
 set -euox pipefail
 
-${BST_NO_PUSH} --max-jobs $(( $(nproc) / 4 )) ${ARCH_OPT} build gnomeos/live-image.bst
-${BST} ${ARCH_OPT} artifact checkout --hardlinks gnomeos/live-image.bst --directory live-image
-
 if [[ -n "${CI_COMMIT_TAG:-}" ]]; then
     aws s3 cp --acl public-read live-image/disk.iso \
         "s3://gnome-build-meta/tag/${CI_COMMIT_TAG}/live_${CI_COMMIT_TAG}-${ARCH}.iso"
