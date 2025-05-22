a4322a7d66bd8ac28c3d45f9c98fe998d6ed1b93
Abderrahim Kitouni <akitouni@gnome.org>
2025-05-22 06:09:22 +0100

    ci: Build GNOME OS on regular (v1) x86_64
    We now have more limited CI capacity, and more importantly limited
    arifact cache capacity
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3757>

--- .gitlab-ci/scripts/build_elements.sh
+++ .gitlab-ci/scripts/build_elements.sh
@@ -5,15 +5,10 @@ set -euxo pipefail
 TARGETS_RUNTIME=(flatpak-runtimes.bst flatpak-platform-extensions.bst flatpak-platform-extensions-extra.bst flatpak/platform-manifest.bst flatpak/sdk-manifest.bst)
 TARGETS_GNOMEOS=(core.bst gnomeos/manifest-devel.bst gnomeos/build-non-images.bst)
 
-# Build the runtime with x86_64_v1 and GNOME OS with x86_64_v3
 # We don't need to build gnomeos on i686
 case "${ARCH}" in
     x86_64)
-        if [[ "${X86_64_V3:-0}" == "1" ]]; then
-            TARGETS=(${TARGETS_GNOMEOS[@]})
-        else
-            TARGETS=(${TARGETS_RUNTIME[@]})
-        fi
+        TARGETS=(${TARGETS_RUNTIME[@]} ${TARGETS_GNOMEOS[@]})
     ;;
     i686)
         TARGETS=(${TARGETS_RUNTIME[@]})
