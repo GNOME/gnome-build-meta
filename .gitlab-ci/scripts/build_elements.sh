82e94c070bf50a30c6396d2c30a368426ea62b46
Jordan Petridis <jpetridis@gnome.org>
2025-06-07 01:55:48 +0300

    build oci images

--- .gitlab-ci/scripts/build_elements.sh
+++ .gitlab-ci/scripts/build_elements.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 TARGETS_RUNTIME=(flatpak-runtimes.bst flatpak-platform-extensions.bst flatpak-platform-extensions-extra.bst flatpak/platform-manifest.bst flatpak/sdk-manifest.bst)
-TARGETS_GNOMEOS=(core.bst gnomeos/manifest-devel.bst gnomeos/build-non-images.bst)
+TARGETS_GNOMEOS=(core.bst gnomeos/manifest-devel.bst gnomeos/build-non-images.bst oci/core.bst)
 
 # We don't need to build gnomeos on i686
 case "${ARCH}" in
