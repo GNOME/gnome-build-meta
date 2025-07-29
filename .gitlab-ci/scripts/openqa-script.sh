6ce97ac6b654a8ed91aa10440c8b1e3d76474024
Sam Thursfield <sam.thursfield@codethink.co.uk>
2025-07-29 20:46:55 +0200

    ci: Update to use openqa tests from this repo
    To avoid adding openqa-utils.git as a submodule, we clone that
    in a separate step.

--- .gitlab-ci/scripts/openqa-script.sh
+++ .gitlab-ci/scripts/openqa-script.sh
@@ -3,8 +3,8 @@
 set -eu
 set -o pipefail
 
-git clone "$OPENQA_TESTS_GIT" --branch "$OPENQA_TESTS_BRANCH" --recurse-submodules ./openqa
-echo "Checked out $OPENQA_TESTS_GIT commit $(git -C ./openqa rev-parse HEAD)"
+git clone "$OPENQA_UTILS_GIT" --branch "$OPENQA_UTILS_BRANCH" --recurse-submodules ./tests/openqa/utils
+echo "Checked out $OPENQA_UTILS_GIT commit $(git -C ./tests/openqa/utils rev-parse HEAD)"
 
 rm --recursive --verbose /etc/openqa/*
 cat >/etc/openqa/client.conf <<EOF
@@ -14,9 +14,9 @@ secret = $OPENQA_API_SECRET
 EOF
 
 # Kludge so we can use `/tests/config/smbios.txt` to locate the smbios file.
-ln -s "$(pwd)/openqa" /tests
+ln -s "$(pwd)/tests/openqa" /tests
 worker_class="qemu_x86_64-${CI_JOB_ID}"
-openqa/utils/setup_worker.sh "${worker_class}"
+tests/openqa/utils/setup_worker.sh "${worker_class}"
 /run_openqa_worker.sh &> worker.log &
 
 echo "Starting jobs: "
@@ -24,7 +24,7 @@ echo
 version="master"
 casedir="$(pwd)/openqa"
 for testsuite in $TESTSUITES; do
-    openqa/utils/start_job.sh "${worker_class}" "${version}" "${casedir}" "${testsuite}" > /tmp/job_id
+    tests/openqa/utils/start_job.sh "${worker_class}" "${version}" "${casedir}" "${testsuite}" > /tmp/job_id
     echo " * ${testsuite}: $OPENQA_HOST/tests/$(cat /tmp/job_id)"
     cat /tmp/job_id >> /tmp/all_job_ids
 done
@@ -32,5 +32,5 @@ done
 # There are multiple ids and we want them to be split instead
 # of being passed as strings
 # shellcheck disable=SC2046
-openqa/utils/wait_for_job.sh $(cat /tmp/all_job_ids) > /tmp/exit_code
+tests/openqa/utils/wait_for_job.sh $(cat /tmp/all_job_ids) > /tmp/exit_code
 exit "$(cat /tmp/exit_code)"
