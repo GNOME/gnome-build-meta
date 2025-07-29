b7c5016fec321f0d7929dbc96184de09d42137ee
Sam Thursfield <sam.thursfield@codethink.co.uk>
2025-07-29 22:47:45 +0200

    ci: Add a separate workflow for openQA test developers
    When modifying the openQA tests themselves, you don't need to rebuild
    the GNOME OS image first -- use the prebuilt one from os.gnome.org so
    CI is fast.

--- .gitlab-ci/scripts/openqa-prepare.sh
+++ .gitlab-ci/scripts/openqa-prepare.sh
@@ -4,10 +4,12 @@ set -eux
 set -o pipefail
 
 echo "Preparing test media for desktop test"
-if [[ -n "${CI_COMMIT_TAG:-}" ]]; then
-    S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_COMMIT_TAG}/live_${CI_COMMIT_TAG}-x86_64.iso
-else
-    S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_PIPELINE_ID}/live_${CI_PIPELINE_ID}-x86_64.iso
+if [[ -z "${S3_DISK_IMAGE_URL:-}" ]]; then
+    if [[ -n "${CI_COMMIT_TAG:-}" ]]; then
+        S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_COMMIT_TAG}/live_${CI_COMMIT_TAG}-x86_64.iso
+    else
+        S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_PIPELINE_ID}/live_${CI_PIPELINE_ID}-x86_64.iso
+    fi
 fi
 
 mkdir -p /data/factory/iso
