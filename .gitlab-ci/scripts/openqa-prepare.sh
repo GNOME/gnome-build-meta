172711a92f69615b5f9bc14cde048974fe850e6b
Jordan Petridis <jpetridis@gnome.org>
2025-09-22 01:36:57 +0300

    ci: Upload iso images from MRs as well
    This allows running test-s3-image (the openQA end-to-end tests) on all
    branches, including open merge requests. Pre-merge openQA testing!
    
    Replaces GNOME/gnome-build-meta!2356
    
    Closes #601
    
    Co-authored-by: Sam Thursfield <sam@afuera.me.uk>
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/3949>

--- .gitlab-ci/scripts/openqa-prepare.sh
+++ .gitlab-ci/scripts/openqa-prepare.sh
@@ -1,14 +1,20 @@
 #! /bin/bash
 
-set -eux
+set -eu
 set -o pipefail
 
 echo "Preparing test media for desktop test"
 if [[ -z "${S3_DISK_IMAGE_URL:-}" ]]; then
-    if [[ -n "${CI_COMMIT_TAG:-}" ]]; then
-        S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_COMMIT_TAG}/live_${CI_COMMIT_TAG}-x86_64.iso
+    if [ "${CI_COMMIT_REF_PROTECTED:-}" = true ]; then
+        if [[ -n "${CI_COMMIT_TAG:-}" ]]; then
+            S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_COMMIT_TAG}/live_${CI_COMMIT_TAG}-x86_64.iso
+        else
+            S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_PIPELINE_ID}/live_${CI_PIPELINE_ID}-x86_64.iso
+        fi
     else
-        S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_PIPELINE_ID}/live_${CI_PIPELINE_ID}-x86_64.iso
+        # Special uri that starts with mr$version and redirects
+        # https://gitlab.gnome.org/Infrastructure/openshift-images/gnome-os-website/-/blob/master/main.py?ref_type=heads#L74
+        S3_DISK_IMAGE_URL=https://os.gnome.org/download/mr${CI_MERGE_REQUEST_IID}/live_mr_${CI_PIPELINE_ID}-${ARCH}.iso
     fi
 fi
 
--- .gitlab-ci/scripts/publish-s3-image.sh
+++ .gitlab-ci/scripts/publish-s3-image.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 
-set -euox pipefail
+set -euo pipefail
 
-if [[ -n "${CI_COMMIT_TAG:-}" ]]; then
-    aws s3 cp --acl public-read live-image/disk.iso \
-        "s3://gnome-build-meta/tag/${CI_COMMIT_TAG}/live_${CI_COMMIT_TAG}-${ARCH}.iso"
+
+if [ "${CI_COMMIT_REF_PROTECTED:-}" = true ]; then
+    if [[ -n "${CI_COMMIT_TAG:-}" ]]; then
+        aws s3 cp --acl public-read live-image/disk.iso \
+            "s3://gnome-build-meta/tag/${CI_COMMIT_TAG}/live_${CI_COMMIT_TAG}-${ARCH}.iso"
+    else
+        aws s3 cp --acl public-read live-image/disk.iso \
+            "s3://gnome-build-meta/nightly/${CI_PIPELINE_ID}/live_${CI_PIPELINE_ID}-${ARCH}.iso"
+    fi
 else
     aws s3 cp --acl public-read live-image/disk.iso \
-        "s3://gnome-build-meta/nightly/${CI_PIPELINE_ID}/live_${CI_PIPELINE_ID}-${ARCH}.iso"
+        "s3://gnome-build-meta/merge_request/mr${CI_MERGE_REQUEST_IID}/live_mr_${CI_PIPELINE_ID}-${ARCH}.iso"
 fi
 
 aws s3 ls --recursive --human-readable s3://gnome-build-meta/
