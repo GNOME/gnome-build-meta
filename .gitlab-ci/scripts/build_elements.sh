978cdeceaf01988b045c6c3f0f136ada90ed044e
Jordan Petridis <jpetridis@gnome.org>
2025-09-22 15:56:14 +0300

    Remove leftover i686 references
    We stopped building i686 compat in cbf34deec8b8bf252d71aee1a15e7e7d14ab9bc4
    and GNOME 49 Runtime doesn't have a i686 compat
    extension at all.
    
    Remove all i686 references left as we no longer support
    it in any way.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4142>

--- .gitlab-ci/scripts/build_elements.sh
+++ .gitlab-ci/scripts/build_elements.sh
@@ -5,13 +5,9 @@ set -euxo pipefail
 TARGETS_RUNTIME=(flatpak-runtimes.bst flatpak-platform-extensions.bst flatpak/platform-manifest.bst flatpak/sdk-manifest.bst)
 TARGETS_GNOMEOS=(core.bst gnomeos/manifest-devel.bst gnomeos/build-non-images.bst)
 
-# We don't need to build gnomeos on i686
 case "${ARCH}" in
     x86_64)
         TARGETS=(${TARGETS_RUNTIME[@]} ${TARGETS_GNOMEOS[@]})
-    ;;
-    i686)
-        TARGETS=(${TARGETS_RUNTIME[@]})
     ;;
     aarch64)
         TARGETS=(${TARGETS_RUNTIME[@]} ${TARGETS_GNOMEOS[@]})
