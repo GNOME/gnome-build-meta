bc54b9fcbba2cf3a546cf73a5d6593cf5ffeb485
Jordan Petridis <jpetridis@gnome.org>
2025-09-24 15:55:51 +0300

    ci: Cache isos we download for openqa in the runners cache

--- .gitlab-ci/scripts/openqa-prepare.sh
+++ .gitlab-ci/scripts/openqa-prepare.sh
@@ -7,17 +7,40 @@ echo "Preparing test media for desktop test"
 if [[ -z "${S3_DISK_IMAGE_URL:-}" ]]; then
     if [ "${CI_COMMIT_REF_PROTECTED-}" = true ]; then
         if [[ -n "${CI_COMMIT_TAG:-}" ]]; then
-            S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_COMMIT_TAG}/live_${CI_COMMIT_TAG}-x86_64.iso
+            DISK_IMAGE_NAME="live_${CI_COMMIT_TAG}-x86_64.iso"
+            S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_COMMIT_TAG}/${DISK_IMAGE_NAME}
         else
-            S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_PIPELINE_ID}/live_${CI_PIPELINE_ID}-x86_64.iso
+            DISK_IMAGE_NAME="live_${CI_PIPELINE_ID}-x86_64.iso"
+            S3_DISK_IMAGE_URL=https://os.gnome.org/download/${CI_PIPELINE_ID}/${DISK_IMAGE_NAME}
         fi
     else
         # Special uri that starts with mr$version and redirects
         # https://gitlab.gnome.org/Infrastructure/openshift-images/gnome-os-website/-/blob/master/main.py?ref_type=heads#L74
-        S3_DISK_IMAGE_URL=https://os.gnome.org/download/mr${CI_MERGE_REQUEST_IID}/live_${CI_MERGE_REQUEST_IID}-${ARCH}.iso
+        DISK_IMAGE_NAME="live_mr_${CI_PIPELINE_ID}-${ARCH}.iso"
+        S3_DISK_IMAGE_URL=https://os.gnome.org/download/mr${CI_MERGE_REQUEST_IID}/${DISK_IMAGE_NAME}
     fi
 fi
 
 mkdir -p /data/factory/iso
-curl --fail --get --location "$S3_DISK_IMAGE_URL" --output /data/factory/iso/disk.iso
+if [[ ! -z "${CI:-}" ]]; then
+    cache_dir="/cache/gnome-build-meta/openqa"
+    mkdir -p $cache_dir
+    ls -l $cache_dir
+    if [ ! -f "$cache_dir/${DISK_IMAGE_NAME}" ]; then
+        # FIXME: grab a lock so we won't race with other jobs
+        curl --fail --get --location "$S3_DISK_IMAGE_URL" --output $cache_dir/${DISK_IMAGE_NAME} 
+    fi
+
+    cp $cache_dir/${DISK_IMAGE_NAME} /data/factory/iso/disk.iso
+
+    # only keep a few isos around
+    n_keep=6
+    cd "$cache_dir"
+    ls -t | tail -n +$n_keep | xargs -r rm --
+    ls -l $cache_dir
+else
+    curl --fail --get --location "$S3_DISK_IMAGE_URL" --output /data/factory/iso/disk.iso
+fi
+
+mkdir -p /data/factory/iso
 sha256sum /data/factory/iso/disk.iso
