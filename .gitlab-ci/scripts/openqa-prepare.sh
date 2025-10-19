dd40e2e0fc89db0a04abe03502ea7e11c9b00905
Jordan Petridis <jpetridis@gnome.org>
2025-10-19 15:04:40 +0300

    openqa-prepare: Log the image url we are downloading
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4209>

--- .gitlab-ci/scripts/openqa-prepare.sh
+++ .gitlab-ci/scripts/openqa-prepare.sh
@@ -19,5 +19,8 @@ if [[ -z "${S3_DISK_IMAGE_URL:-}" ]]; then
 fi
 
 mkdir -p /data/factory/iso
+
+echo "Downloading image from: $S3_DISK_IMAGE_URL"
 curl --fail --get --location "$S3_DISK_IMAGE_URL" --output /data/factory/iso/disk.iso
+
 sha256sum /data/factory/iso/disk.iso
