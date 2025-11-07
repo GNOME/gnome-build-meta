9f42647c7c1884dfe653b58959af1b213b8738eb
Jordan Petridis <jpetridis@gnome.org>
2025-11-07 23:19:53 +0200

    oci: Make the gnomeos images bootc compatible
    Co-Authored-By: Valentin David <me@valentindavid.com>

--- files/gnomeos/generate-initramfs/modules/50-live/module.sh
+++ files/gnomeos/generate-initramfs/modules/50-live/module.sh
@@ -1,11 +1,17 @@
 install() {
+    if [ "${INITRD_MODE-sysupdate}" != sysupdate ]; then
+        exit 0
+    fi
+
     install_files \
         /usr/bin/systemd-detect-virt \
         /usr/lib/systemd/systemd-makefs \
         /usr/lib/systemd/system-generators/gnomeos-live \
         /usr/lib/udev/rules.d/90-ramfs-root.rules \
         /usr/lib/systemd/system-generators/zram-generator \
-        /usr/lib/systemd/system/systemd-zram-setup@.service
+        /usr/lib/systemd/system/systemd-zram-setup@.service \
+        /usr/lib/systemd/system/systemd-repart.service.d/live.conf \
+        /usr/lib/systemd/system/systemd-tpm2-setup-early.service.d/live.conf
 
     for path in $(modinfo -k "${kernelver}" -b /usr -n zram); do
         case "${path}" in
