1b941d854c157ebf66a330c5a1cc2e3615a7f282
Valentin David <me@valentindavid.com>
2025-12-20 19:26:19 +0100

    utils/run-secure-vm.sh: Add vnc display

--- utils/run-secure-vm.sh
+++ utils/run-secure-vm.sh
@@ -82,6 +82,8 @@ Options:
 
   --gtk                      Use gtk ui.
 
+  --vnc                      Run as a vnc server.
+
   --home-disk PATH           Also mount home disk as an image file
 
   --home-disk-key PATH       Use public key for signing home disk. By default,
@@ -150,10 +152,13 @@ while [ $# -gt 0 ]; do
             local_updates=1
             ;;
         --spice-app)
-            spice_app=1
+            display=spice
+            ;;
+        --vnc)
+            display=vnc
             ;;
         --gtk)
-            gtk=1
+            display=gtk
             ;;
         --home-disk)
             shift
@@ -372,24 +377,31 @@ fi
 
 QEMU_ARGS+=(-name "${VM_NAME}")
 
-QEMU_ARGS+=(-device virtio-vga-gl)
-
-if [ "${spice_app+set}" != set ] && [ "${gtk+set}" != set ]; then
+if [ "${display+set}" != set ]; then
     if [ "$(gdbus call --session --dest org.freedesktop.portal.Desktop --object-path /org/freedesktop/portal/desktop --method org.freedesktop.portal.OpenURI.SchemeSupported "spice" {})" = "(true,)" ]; then
-        spice_app=1
+        display=spice
     else
-        gtk=1
+        display=gtk
     fi
 fi
 
-if [ "${spice_app+set}" = set ]; then
-    QEMU_ARGS+=(-display spice-app,gl=on)
-    QEMU_ARGS+=(-device virtio-serial-pci
-                -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0
-                -chardev spicevmc,id=spicechannel0,name=vdagent)
-else
-    QEMU_ARGS+=(-display gtk,gl=on)
-fi
+case "${display-gtk}" in
+    spice)
+        QEMU_ARGS+=(-device virtio-vga-gl)
+        QEMU_ARGS+=(-display spice-app,gl=on)
+        QEMU_ARGS+=(-device virtio-serial-pci
+                    -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0
+                    -chardev spicevmc,id=spicechannel0,name=vdagent)
+        ;;
+    gtk)
+        QEMU_ARGS+=(-device virtio-vga-gl)
+        QEMU_ARGS+=(-display gtk,gl=on)
+        ;;
+    vnc)
+        QEMU_ARGS+=(-device virtio-vga)
+        QEMU_ARGS+=(-display vnc=:0)
+        ;;
+esac
 
 QEMU_ARGS+=(-device ich9-intel-hda)
 QEMU_ARGS+=(-audiodev pa,id=sound0)
