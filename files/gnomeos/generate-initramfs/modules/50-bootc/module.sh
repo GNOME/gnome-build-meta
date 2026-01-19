dfb193809ff5dac5f9c0794c5080475f9ff58864
Jordan Petridis <jpetridis@gnome.org>
2026-01-19 15:24:48 +0200

    oci/gnomeos: Add ostree to the intiramfs as well
    We already have bootc with composefs, but if we
    add ostree as well it will allow bootc to also use
    that backend if needed. (Example: rebasing from ostree
    image to bootc)
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4448>

--- files/gnomeos/generate-initramfs/modules/50-bootc/module.sh
+++ files/gnomeos/generate-initramfs/modules/50-bootc/module.sh
@@ -5,8 +5,11 @@ install() {
 
     install_files \
         /usr/lib/bootc/initramfs-setup \
+        /usr/lib/ostree/ostree-prepare-root \
+        /usr/lib/systemd/system/ostree-prepare-root.service \
         /usr/lib/systemd/system/bootc-root-setup.service
 
     mkdir -p "${root}/usr/lib/systemd/system/initrd-root-fs.target.wants"
     ln -s ../bootc-root-setup.service "${root}/usr/lib/systemd/system/initrd-root-fs.target.wants/bootc-root-setup.service"
+    ln -s ../ostree-prepare-root.service "${root}/usr/lib/systemd/system/initrd-root-fs.target.wants/ostree-prepare-root.service"
 }
