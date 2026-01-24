9841cbd5931e567ee191b7c7c4b0e8002112ba93
Valentin David <me@valentindavid.com>
2026-01-24 12:25:00 +0100

    lvm2: Disable event activation by default
    Because lvm2 does not have a distro wide configuration in /usr and all
    is in /etc, configuring it is not compatible with systemd-sysupdate
    OSes - because systemd-udevd starts before systemd-tmpfiles. Also on
    GNOME OS, installation is confusing for the user that cannot format a
    disk before they deactivate the volume groups taking phyisical volumes
    from that disk.
    
    The solution is to disable this by default.
    
    Now, Bluefin users will be fine with lvm2 installations, as
    bootc/OSTree still provides /etc at the right time. So for those
    distros we will set the initial value in /etc to activate the groups.
    
    It does not seem Endless OS uses lvm2 volume groups. Furthermore, it
    could also set the configuration in /etc since it uses OSTree.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4462>

--- files/gnomeos/generate-initramfs/modules/50-udev/module.sh
+++ files/gnomeos/generate-initramfs/modules/50-udev/module.sh
@@ -1,11 +1,24 @@
 install() {
-  for rule in /usr/lib/udev/rules.d/*.rules; do
-      install_file "${rule}"
-  done
-  for command in /usr/lib/udev/*; do
-      if ! [ -d "${command}" ] && [ -x "${command}" ]; then
-          install_file "${command}"
-      fi
-  done
-  install_files /usr/bin/dmsetup /usr/bin/lvm
+    for rule in /usr/lib/udev/rules.d/*.rules; do
+        case "$(basename "${rule}")" in
+            *-dm-lvm.rules)
+                if [ "${INITRD_MODE-sysupdate}" != sysupdate ]; then
+                    install_file "${rule}"
+                fi
+                ;;
+            *)
+                install_file "${rule}"
+                ;;
+        esac
+    done
+    for command in /usr/lib/udev/*; do
+        if ! [ -d "${command}" ] && [ -x "${command}" ]; then
+            install_file "${command}"
+        fi
+    done
+    install_file /usr/bin/dmsetup
+    if [ "${INITRD_MODE-sysupdate}" != sysupdate ]; then
+        # /etc configuration might override "event-activation"
+        install_files /usr/bin/lvm /etc/lvm/lvm.conf
+    fi
 }
