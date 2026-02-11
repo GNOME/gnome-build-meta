17b846069c95b97f4cbbc3bfae841dc4e6662703
Valentin David <me@valentindavid.com>
2026-02-11 21:14:31 +0100

    gnomeos: Make /etc the writable layer
    This makes /etc instead of /var/lib/extensions.mutable/etc the
    writable layer. This is supported by systemd-confext. See the
    "mutabilty" section of the man page.
    
    Suggested by Kai Lüke.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4536>

--- files/gnomeos/generate-initramfs/modules/50-confext/module.sh
+++ files/gnomeos/generate-initramfs/modules/50-confext/module.sh
@@ -6,10 +6,13 @@ install() {
     install_files \
         "/usr/lib/systemd/system/initrd-confext.service" \
         "/usr/lib/systemd/system/initrd-migrate-confext.service" \
-        "/usr/lib/gnomeos/migrate-confext"
+        "/usr/lib/gnomeos/migrate-confext" \
+        "/usr/lib/systemd/system/initrd-create-mutable-etc.service" \
+        "/usr/lib/tmpfiles.d/mutable-etc.conf"
 
     mkdir -p "${root}/usr/lib/systemd/system/initrd-fs.target.wants"
 
     ln -s ../initrd-confext.service "${root}/usr/lib/systemd/system/initrd.target.wants/initrd-confext.service"
     ln -s ../initrd-migrate-confext.service "${root}/usr/lib/systemd/system/initrd.target.wants/initrd-migrate-confext.service"
+    ln -s ../initrd-create-mutable-etc.service "${root}/usr/lib/systemd/system/initrd.target.wants/initrd-create-mutable-etc.service"
 }
