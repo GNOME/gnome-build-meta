GNOME/gnome-build-meta

commit 42d0271b29992eeddf8fc0113526a0437cbe34ff

gnomeos: Add safe-mode boot entry
Inspired by @ada-magicat's PR !3953. But here we have a generator that
correctly sets the module configuration for nvidia only not in safe mode.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4608>

files/gnomeos/live/generator.c
@@ -39,6 +39,24 @@ static void fclosep(FILE **p) {
         }
 }
 
+static bool mask_unit(const char *generator_path, const char* name) {
+        _cleanup_(freestr) char* path = NULL;
+        int r;
+
+        if (0 > asprintf(&path, "%s/%s", generator_path, name)) {
+                kmsg(3, "Could not format string\n");
+                return false;
+        }
+
+        r = symlink("/dev/null", path);
+        if (r < 0) {
+                kmsg(3, "Cannot symlink %s: %s\n", path, STRERROR(errno));
+                return false;
+        }
+
+        return true;
+}
+
 static bool generate_unit(const char *generator_path, const char* name, const char *format, ...) __attribute__ ((format (printf, 3, 4)));
 
 static bool generate_unit(const char *generator_path, const char* name, const char *format, ...) {
@@ -95,7 +113,7 @@ static bool enable_unit(const char *generator_path, const char *from, const char
         return true;
 }
 
-static void mark_live() {
+static bool mark_live() {
         /*
          * This is a marker we can easily check in polkit rules.
          * See files/gnomeos/proto-installer/org.gnome.Installer1.rules
@@ -103,27 +121,16 @@ static void mark_live() {
         _cleanup_(closep) int fd = -EBADF;
         mkdir("/run/gnomeos", 0777);
         fd = open("/run/gnomeos/is-live", O_CREAT|O_WRONLY|O_TRUNC, 0666);
-}
-
-int main(int argc, char *argv[]) {
-        _cleanup_(fclosep) FILE* file = NULL;
-        char *in_initrd_str;
-        int in_initrd;
-        bool is_live;
-
-        if (argc != 4) {
-                kmsg(3, "Wrong number of parameters\n");
-                return 1;
+        if (fd < 0) {
+                kmsg(3, "Cannot create /run/gnomeos/is-live: %s\n", STRERROR(errno));
+                return false;
         }
 
-        if (!parse_cmdline(&is_live))
-                return 1;
+        return true;
+}
 
-        if (!is_live)
-                return 0;
-
-        in_initrd_str = getenv("SYSTEMD_IN_INITRD");
-        in_initrd = in_initrd_str && (strcmp(in_initrd_str, "") != 0) && (strcmp(in_initrd_str, "0") != 0);
+static bool handle_live(const char *generator_path, bool in_initrd) {
+        _cleanup_(fclosep) FILE* file = NULL;
 
         if (in_initrd) {
                 mkdir("/run/systemd/zram-generator.conf.d", 0777);
@@ -131,21 +138,26 @@ int main(int argc, char *argv[]) {
                 fprintf(file, "[zram1]\nmount-point=/sysroot\nfs-type=btrfs\n");
                 fclosep(&file);
 
-                if (!enable_unit(argv[1], "initrd-root-device.target.wants", "systemd-zram-setup@zram1.service", "/usr/lib/systemd/system"))
-                        return 1;
+                if (!enable_unit(generator_path, "initrd-root-device.target.wants", "systemd-zram-setup@zram1.service", "/usr/lib/systemd/system"))
+                        return false;
 
-                if (!enable_unit(argv[1], "initrd-root-fs.target.wants", "sysroot.mount", ".."))
-                        return 1;
+                if (!enable_unit(generator_path, "initrd-root-fs.target.wants", "sysroot.mount", ".."))
+                        return false;
 
         } else {
                 mkdir("/run/systemd/zram-generator.conf.d", 0777);
                 file = fopen("/run/systemd/zram-generator.conf.d/zram1.conf", "w");
+                if (!file) {
+                        kmsg(3, "Cannot write /run/systemd/zram-generator.conf.d/zram1.conf: %s\n", STRERROR(errno));
+                        return false;
+                }
                 fprintf(file, "[zram1]\nmount-point=/\nfs-type=btrfs\n");
                 fclosep(&file);
 
-                mark_live();
+                if (!mark_live())
+                        return false;
 
-                if (!generate_unit(argv[1], "var-lib-gnomeos-installer\\x2desp.mount",
+                if (!generate_unit(generator_path, "var-lib-gnomeos-installer\\x2desp.mount",
                                    "[Unit]\n"
                                    "BindsTo=dev-gnomeos\\x2dinstaller-esp.device\n"
                                    "After=dev-gnomeos\\x2dinstaller-esp.device\n"
@@ -155,20 +167,20 @@ int main(int argc, char *argv[]) {
                                    "Where=/var/lib/gnomeos/installer-esp\n"
                                    "Type=vfat\n"
                                    "Options=fmask=0177,dmask=0077,ro,nodev,nosuid,noexec,nosymfollow\n"))
-                        return 1;
+                        return false;
 
-                if (!generate_unit(argv[1], "var-lib-gnomeos-installer\\x2desp.automount",
+                if (!generate_unit(generator_path, "var-lib-gnomeos-installer\\x2desp.automount",
                                    "[Automount]\n"
                                    "Where=/var/lib/gnomeos/installer-esp\n"
                                    "TimeoutIdleSec=120\n"))
-                        return 1;
+                        return false;
 
 
-                if (!enable_unit(argv[1], "local-fs.target.wants", "var-lib-gnomeos-installer\\x2desp.automount", ".."))
-                        return 1;
+                if (!enable_unit(generator_path, "local-fs.target.wants", "var-lib-gnomeos-installer\\x2desp.automount", ".."))
+                        return false;
 
                 /* This is needed for the installer. We just prepare it, so it is loaded. But we not enable it. */
-                if (!generate_unit(argv[1], "systemd-cryptsetup@root.service",
+                if (!generate_unit(generator_path, "systemd-cryptsetup@root.service",
                                    "[Unit]\n"
                                    "Description=Cryptography Setup for %%I\n"
                                    "\n"
@@ -190,10 +202,10 @@ int main(int argc, char *argv[]) {
                                    "OOMScoreAdjust=500\n"
                                    "ExecStart=/usr/bin/systemd-cryptsetup attach root /dev/gnomeos-pab-install/root-luks\n"
                                    "ExecStop=/usr/bin/systemd-cryptsetup detach root\n"))
-                        return 1;
+                        return false;
 
                 /* This is needed for the installer. We just prepare it, so it is loaded. But we not enable it. */
-                if (!generate_unit(argv[1], "efi.mount",
+                if (!generate_unit(generator_path, "efi.mount",
                                    "[Unit]\n"
                                    "BindsTo=dev-gnomeos\\x2dpab\\x2dinstall-esp.device\n"
                                    "After=dev-gnomeos\\x2dpab\\x2dinstall-esp.device\n"
@@ -203,15 +215,73 @@ int main(int argc, char *argv[]) {
                                    "Where=/efi\n"
                                    "Type=vfat\n"
                                    "Options=fmask=0177,dmask=0077,rw,nodev,nosuid,noexec,nosymfollow\n"))
-                        return 1;
+                        return false;
 
                 /* This is needed for the installer. We just prepare it, so it is loaded. But we not enable it. */
-                if (!generate_unit(argv[1], "efi.automount",
+                if (!generate_unit(generator_path, "efi.automount",
                                    "[Automount]\n"
                                    "Where=/efi\n"
                                    "TimeoutIdleSec=120\n"))
-                        return 1;
+                        return false;
         }
+
+        return true;
+}
+
+static bool handle_safe(const char *generator_path, bool in_initrd) {
+        if (in_initrd) {
+                if (!mask_unit(generator_path, "systemd-sysext-initrd.service"))
+                        return false;
+        } else {
+                if (!mask_unit(generator_path, "systemd-sysext.service"))
+                        return false;
+        }
+
+        return true;
+}
+
+static bool handle_nvidia(const char *generator_path) {
+        _cleanup_(fclosep) FILE* file = NULL;
+
+        mkdir("/run/modprobe.d", 0777);
+        file = fopen("/run/modprobe.d/gnomeos-nvidia.conf", "w");
+        if (!file) {
+                kmsg(3, "Cannot write /run/modprobe.d/gnomeos-nvidia.conf: %s\n", STRERROR(errno));
+                return false;
+        }
+        fprintf(file, "blacklist nouveau\noptions nvidia_drm modeset=1 NVreg_PreserveVideoMemoryAllocations=1\n");
+        fclosep(&file);
+
+        return true;
+}
+
+int main(int argc, char *argv[]) {
+        char *in_initrd_str;
+        int in_initrd;
+        bool is_live, is_safe, is_nvidia;
+
+        if (argc != 4) {
+                kmsg(3, "Wrong number of parameters\n");
+                return 1;
+        }
+
+        if (!parse_cmdline(&is_live, &is_safe, &is_nvidia))
+                return 1;
+
+        in_initrd_str = getenv("SYSTEMD_IN_INITRD");
+        in_initrd = in_initrd_str && (strcmp(in_initrd_str, "") != 0) && (strcmp(in_initrd_str, "0") != 0);
+
+        if (is_live)
+                if (!handle_live(argv[1], in_initrd))
+                        return 1;
+
+        if (is_safe)
+                if (!handle_safe(argv[1], in_initrd))
+                        return 1;
+
+        if (!is_safe && is_nvidia)
+                if (!handle_nvidia(argv[1]))
+                                return 1;
 
         return 0;
 }

elements/gnomeos/signed-boot.bst
@@ -67,7 +67,7 @@ config:
     ukify build \
       --secureboot-private-key=VENDOR.key \
       --secureboot-certificate=VENDOR.crt \
-      --cmdline='module_blacklist=nouveau nvidia_drm.modeset=1 nvidia.NVreg_PreserveVideoMemoryAllocations=1' \
+      --cmdline='gnomeos.nvidia-mode=1' \
       --sbat=@sbat.txt \
       --output="%{install-root}/efi/EFI/Linux/gnomeos_%{image-version}.efi.extra.d/nvidia.addon.efi"
 
@@ -84,6 +84,11 @@ config:
         )
       ;;
     esac
+
+    ukify build \
+      --profile="TITLE=Safe mode" \
+      --cmdline="%{kcmdline} usrhash=${roothash} gnomeos.safe-mode=1" \
+      --output="profile-safe-mode.efi"
 
     # Note: Our security model allows for an attacker to replace the rootfs
     # with a maliciously crafted one. This means that it's only safe to
@@ -117,6 +122,7 @@ config:
       --secureboot-certificate=VENDOR.crt \
       --linux="/boot/vmlinuz" \
       --initrd="/usr/lib/modules/${version}/initramfs.img" \
+      --join-profile="profile-safe-mode.efi" \
       "${MICROCODE[@]}" "${DEVICETREES[@]}"
 
 sources:

files/gnomeos/generate-initramfs/modules/50-live/module.sh
@@ -6,7 +6,7 @@ install() {
     install_files \
         /usr/bin/systemd-detect-virt \
         /usr/lib/systemd/systemd-makefs \
-        /usr/lib/systemd/system-generators/gnomeos-live \
+        /usr/lib/systemd/system-generators/gnomeos-generator \
         /usr/lib/udev/rules.d/90-ramfs-root.rules \
         /usr/lib/systemd/system-generators/zram-generator \
         /usr/lib/systemd/system/systemd-zram-setup@.service \

files/gnomeos/live/env-generator.c
@@ -9,7 +9,7 @@ int main() {
         const char* original;
         bool is_live;
 
-        if (!parse_cmdline(&is_live))
+        if (!parse_cmdline(&is_live, NULL, NULL))
                 return 1;
 
         if (!is_live)

files/gnomeos/live/kernel-cmdline.c
@@ -115,7 +115,7 @@ static char *kernel_cmdline() {
         return strdup(buf);
 }
 
-bool parse_cmdline(bool *is_live) {
+bool parse_cmdline(bool *is_live, bool *is_safe, bool *is_nvidia) {
         _cleanup_(freestr) char* cmdline = NULL;
         char *current, *param, *val;
 
@@ -123,14 +123,32 @@ bool parse_cmdline(bool *is_live) {
         if (!cmdline)
                 return false;
 
-        *is_live = false;
+        if (is_live)
+                *is_live = false;
+        if (is_safe)
+                *is_safe = false;
+        if (is_nvidia)
+                *is_nvidia = false;
 
         current = cmdline;
         while (*current) {
                 current = kernel_next_arg(current, &param, &val);
                 if (strcmp(param, "root") == 0) {
                         if (strcmp(val, "live:gnomeos") == 0) {
-                                *is_live = true;
+                                if (is_live)
+                                        *is_live = true;
+                        }
+                }
+                if (strcmp(param, "gnomeos.safe-mode") == 0) {
+                        if (strcmp(val, "1") == 0) {
+                                if (is_safe)
+                                        *is_safe = true;
+                        }
+                }
+                if (strcmp(param, "gnomeos.nvidia-mode") == 0) {
+                        if (strcmp(val, "1") == 0) {
+                                if (is_nvidia)
+                                        *is_nvidia = true;
                         }
                 }
         }

files/gnomeos/live/kernel-cmdline.h
@@ -3,6 +3,6 @@
 
 #include <stdbool.h>
 
-bool parse_cmdline(bool *is_live);
+bool parse_cmdline(bool *is_live, bool *is_safe, bool *is_nvidia);
 
 #endif

files/gnomeos/live/meson.build
@@ -22,7 +22,7 @@ common = static_library(
 )
 
 executable(
-  'gnomeos-live',
+  'gnomeos-generator',
   [
     'generator.c',
   ],

commit 786429bb73a984e26713acee457458420e191bf0

files/gnomeos/live/generator.c: Fix nvidia options for supsend
Fixes bug introduced in 42d0271b29992eeddf8fc0113526a0437cbe34ff

Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4685>

files/gnomeos/live/generator.c
@@ -249,7 +249,7 @@ static bool handle_nvidia(const char *generator_path) {
                 kmsg(3, "Cannot write /run/modprobe.d/gnomeos-nvidia.conf: %s\n", STRERROR(errno));
                 return false;
         }
-        fprintf(file, "blacklist nouveau\noptions nvidia_drm modeset=1 NVreg_PreserveVideoMemoryAllocations=1\n");
+        fprintf(file, "blacklist nouveau\noptions nvidia_drm modeset=1\noptions nvidia NVreg_PreserveVideoMemoryAllocations=1\n");
         fclosep(&file);
 
         return true;