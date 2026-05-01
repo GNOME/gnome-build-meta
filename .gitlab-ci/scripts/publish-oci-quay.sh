c9b1e57ef9d6fc9c8d3ebc085d39e07bfe121fa5
Jordan Petridis <jpetridis@gnome.org>
2026-05-01 15:53:33 +0300

    oci: Tag images after the commit for Nightly
    Once upon a time, we used to tag every image after the commit that
    produced it. We stopped doing this for two reasons:
    
    First we had nobody using the tagged images and everyone was on the
    moving tags, example nightly, gnome-50, as they rarerly caused any issue
    
    Second, we didn't have a good cleanup policy and we have ended up with a
    couple thousands tags in the registry.
    
    Start tagging the nightly builds after the commit so they are pinnable
    and downgradable if needed. Stable ones we can ignore for now, we can
    evaluate the need for those later.
    
    Related https://gitlab.gnome.org/GNOME/citemplates/-/merge_requests/179
    
    Partial revert of ed2be4d83baeba6c0c84893bb795b7e0a0070024
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4709>

--- .gitlab-ci/scripts/publish-oci-quay.sh
+++ .gitlab-ci/scripts/publish-oci-quay.sh
@@ -9,7 +9,7 @@ set -x
 tags=("$OCI_BRANCH")
 
 if [ "$OCI_BRANCH" = "master" ]; then
-    tags+=("latest" "nightly")
+    tags+=("$CI_COMMIT_SHA" "latest" "nightly")
 fi
 
 for tag in "${tags[@]}"; do
