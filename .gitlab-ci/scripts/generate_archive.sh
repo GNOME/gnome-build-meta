cddabdc5cb17ce2939042a03647b30dd23324d1d
Jordan Petridis <jpetridis@gnome.org>
2026-05-30 17:01:15 +0300

    ci: Generate and append the NEWS file to the tarball
    While we don't have a news file in the repo, we can start appending
    the file we generate by hand and upload into the dist tarball instead.
    
    This commits the generate_news.py script we had in the snippet into
    the repo and adds some extra python glue to automatically handle the
    discovery of the previous version.
    
    Part of #1135
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4874>

--- .gitlab-ci/scripts/generate_archive.sh
+++ .gitlab-ci/scripts/generate_archive.sh
@@ -4,10 +4,21 @@ set -eu
 
 project_name="gnome-build-meta"
 # Hardcoded test version if it's not set
-version=${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}
+version=${CI_COMMIT_TAG:-48.4}
 
-echo "Creating tar archive"
+echo "Packing the tarball.."
 git archive --prefix "$project_name-$version/" -o "$project_name-$version.tar" "$version"
+
+echo "Generating News.."
+python3 .gitlab-ci/scripts/write_news.py --version "$version"
+
+echo "Appending NEWS file into the tar archive"
+# tar --append --file="$project_name-$version.tar" NEWS
+# --append in tar would put the file in the root directory. There is --transform but it involved regex
+# It's simpler to just let git redo the archive again. Ideally we'd have a way to commit the NEWS file
+# before hand.
+rm "gnome-build-meta-$version.tar"
+git archive --prefix "$project_name-$version/" --add-file NEWS -o "$project_name-$version.tar" "$version"
 
 echo "Compressing archive"
 xz "$project_name-$version.tar"
