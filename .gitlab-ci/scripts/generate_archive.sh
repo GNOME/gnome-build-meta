48d0ee7a06f774ef5dc5ab46777b3ba0093782f1
Jordan Petridis <jpetridis@gnome.org>
2026-05-30 17:01:15 +0300

    ci: Move archive generation into a script
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4874>

---
+++ .gitlab-ci/scripts/generate_archive.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -eu
+
+project_name="gnome-build-meta"
+# Hardcoded test version if it's not set
+version=${CI_COMMIT_TAG:-$CI_COMMIT_SHORT_SHA}
+
+echo "Creating tar archive"
+git archive --prefix "$project_name-$version/" -o "$project_name-$version.tar" "$version"
+
+echo "Compressing archive"
+xz "$project_name-$version.tar"
