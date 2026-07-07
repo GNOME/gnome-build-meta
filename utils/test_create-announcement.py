cb64e95e6eaf39f37cc2753118de2e9570297076
Jordan Petridis <jpetridis@gnome.org>
2026-07-08 00:07:36 +0300

    utils/create_announcement: Write tests
    And also rename the script to be actual python
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4844>

---
+++ utils/test_create-announcement.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+import pytest
+from create_announcement import create_stable_announcement
+
+expected_48_9 = """
+Hi,
+
+GNOME 48.9 is now available!
+
+This is a stable bugfix release for GNOME 48. All operating systems shipping GNOME 48 are encouraged to upgrade.
+
+* Review the list of [updated modules and changes](https://download.gnome.org/sources/gnome-build-meta/48/gnome-build-meta-48.9.news).
+* Use [the official BuildStream project snapshot](https://download.gnome.org/sources/gnome-build-meta/48/gnome-build-meta-48.9.tar.xz) to compile GNOME 48.9.
+
+GNOME 48.9 is designed to be a boring bugfix update for GNOME 48 so it should be a safe and uneventful upgrade from earlier versions of GNOME 48.
+
+Enjoy! ❤️
+GNOME Release Team
+"""
+
+expected_48_10 = """
+Hi,
+
+GNOME 48.10 is now available!
+
+This is a stable bugfix release for GNOME 48. All operating systems shipping GNOME 48 are encouraged to upgrade.
+
+This is the final release of GNOME 48. Please upgrade to GNOME 49 or GNOME 50.
+
+* Review the list of [updated modules and changes](https://download.gnome.org/sources/gnome-build-meta/48/gnome-build-meta-48.10.news).
+* Use [the official BuildStream project snapshot](https://download.gnome.org/sources/gnome-build-meta/48/gnome-build-meta-48.10.tar.xz) to compile GNOME 48.10.
+
+GNOME 48.10 is designed to be a boring bugfix update for GNOME 48 so it should be a safe and uneventful upgrade from earlier versions of GNOME 48.
+
+Enjoy! ❤️
+GNOME Release Team
+"""
+
+def test_create_stable_announcement():
+    r48_9 = create_stable_announcement(48, 9, False)
+    assert r48_9 == expected_48_9
+    r48_10 = create_stable_announcement(48, 10, True)
+    assert r48_10 == expected_48_10
+
+    # .0 announcments are skipped as we handle the release notes through press releases
+    assert create_stable_announcement(48, 0, False) == "" 
