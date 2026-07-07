af356fe3f8d787df03aba6db0512246ee7badeb9
Jordan Petridis <jpetridis@gnome.org>
2026-07-08 00:34:24 +0300

    utils/create_announcement: Publish stable announcments
    Stable announcments are good to go so let's start publishing them on
    discourse directly.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4844>

--- utils/create_announcement.py
+++ utils/create_announcement.py
@@ -3,6 +3,23 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import argparse
+import os
+from typing import List
+
+
+def create_discourse_post(content: str, title: str, category_id: int, tags: List[str]):
+    from pydiscourse import DiscourseClient
+
+    api_username = os.getenv("DISCOURSE_API_USERNAME")
+    api_key = os.getenv("DISCOURSE_API_KEY")
+
+    client = DiscourseClient(
+        "https://discourse.gnome.org", api_username=api_username, api_key=api_key
+    )
+    topic = client.create_post(content, title=title, category_id=category_id, tags=tags)
+
+    return topic
+
 
 def is_stable(major: int, minor: str) -> bool:
     major = int(major)
@@ -22,17 +39,20 @@ def is_stable(major: int, minor: str) -> bool:
 
     raise ValueError("Run out of numbers")
 
-def create_stable_announcement(major: int, minor: int) -> str:
+
+# Returns a tuple of strings, where its (subject, content)
+def create_stable_announcement(major: int, minor: int) -> (str, str):
     # .0 announcments are skipped as we handle the release notes through press releases
     if minor == 0:
-        return ""
+        return ("", "")
 
     # This hardcodes that we only support 2 versions at a time
     final_notice = ""
     if minor == 10:
         final_notice = f"\nThis is the final release of GNOME {major}. Please upgrade to GNOME {major + 1} or GNOME {major + 2}.\n"
 
-    stable_txt = f"""
+    subject = f"GNOME {major}.{minor} is released"
+    content = f"""
 Hi,
 
 GNOME {major}.{minor} is now available!
@@ -48,16 +68,24 @@ Enjoy! ❤️
 GNOME Release Team
 """
 
-    return stable_txt
+    return (subject, content)
 
 
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("version", type=str, help="The GNOME version. ex: 47.8")
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        default=False,
+        help="Print the contents instead of publishing to discourse",
+    )
     args = parser.parse_args()
 
+    dry_run = args.dry_run
     full_version = args.version.split(".")
+
     major = int(full_version[0])
     minor = str(full_version[1])
 
@@ -69,18 +97,26 @@ def main():
 
     stable = is_stable(major, minor)
 
+    # 612 is the ID of the Announcment category on GNOME Discourse
+    # https://discourse.gnome.org/c/news-and-events/announcements/612
+    # https://discourse.gnome.org/tags/c/news-and-events/announcements/612/release-team/52
+    discource_category = 612
+    discource_tags = ["release-team"]
+
     if stable:
-        print(create_stable_announcement(major, minor))
-
-    # TODO:
-    # alpha/beta/rc and .0 are slightly different
-    # notably we usually include a screenshot of gnome tour and a link to an .iso for those versions
-    # additionally alpha/beta/rc also have a big Pre-release warning in the announcement
+        (subject, content) = create_stable_announcement(major, minor)
     else:
-        pass
+        # TODO:
+        # alpha/beta/rc and .0 are slightly different
+        # notably we usually include a screenshot of gnome tour and a link to an .iso for those versions
+        # additionally alpha/beta/rc also have a big Pre-release warning in the announcement
+        (subject, content) = "", ""
 
-    # TODO:
-    # Have CI create the announcement after tagging and publish it using the discourse API
+    if dry_run or not stable:
+        print(f"Subject: {subject}")
+        print(f"Content: {content}")
+    else:
+        create_discourse_post(content, subject, discource_category, discource_tags)
 
 
 if __name__ == "__main__":
