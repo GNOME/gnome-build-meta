dc3a169b3fd353c62c4b1ebff6fe531586765f61
Valentin David <me@valentindavid.com>
2026-08-29 15:11:34 +0200

    utils/run-secure-vm.sh: Work-around spice not being detected as boot console
    Without it, plymouth does not show in spice.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-build-meta/-/merge_requests/4970>

--- utils/run-secure-vm.sh
+++ utils/run-secure-vm.sh
@@ -403,6 +403,8 @@ if [ "${spice_app+set}" = set ]; then
     QEMU_ARGS+=(-device virtio-serial-pci
                 -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0
                 -chardev spicevmc,id=spicechannel0,name=vdagent)
+    # Work-around spice not being detected as a boot console
+    cmdline+=("console=tty0")
 else
     QEMU_ARGS+=(-display gtk,gl=on)
 fi
